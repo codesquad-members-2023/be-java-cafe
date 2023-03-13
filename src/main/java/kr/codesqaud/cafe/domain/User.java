7beca5eb95188eb17752c43009c11dadb4c08ba0
Life is a ninanino <tvxqinak@naver.com>
2023-03-13 21:04:35 +0900

    feat : user class
    회원의 기본 정보가 담겨있다

---
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -0,0 +1,44 @@
+package kr.codesqaud.cafe.domain;
+
+
+public class User {
+    private String userId;
+    private String password;
+    private String userName;
+    private String email;
+
+    public User() {
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
