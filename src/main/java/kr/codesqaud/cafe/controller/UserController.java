c9c0715f3b355b3c9c104cff0dd7b3bdb6e9943f
Hyun <ghkdgus29@naver.com>
2023-03-13 17:07:42 +0900

    feat: 회원목록에서 사용자 아이디를 클릭하면 세부정보를 확인하도록 구현

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -44,5 +45,13 @@ public class UserController {
         model.addAttribute("users", users);
 
         return "user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String showUserProfile(@PathVariable String userId, Model model) {
+        User user = repository.findByUserId(userId);
+        model.addAttribute("user", user);
+
+        return "user/profile";
     }
 }
