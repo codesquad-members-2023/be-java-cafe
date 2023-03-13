02554a78ba562900ce42203b1a9130e73d328dd2
Hyun <ghkdgus29@naver.com>
2023-03-13 14:25:47 +0900

    feat: 회원가입한 후 목록을 조회하도록 UserController 에 매핑 추가
    signUp 메서드의 경우, post를 통해 파라미터가 제대로 오는지 확인하기위해 출력을 찍었다.

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -2,6 +2,8 @@ package kr.codesqaud.cafe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -15,5 +17,25 @@ public class UserController {
     public String showRegistrationForm() {
 
         return "user/form";
+    }
+
+    @PostMapping("/users")
+    public String signUp(@RequestParam String userId,
+                         @RequestParam String password,
+                         @RequestParam String name,
+                         @RequestParam String email) {
+
+        System.out.println("userId = " + userId);
+        System.out.println("password = " + password);
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
+
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String showUsers() {
+
+        return "user/list";
     }
 }
