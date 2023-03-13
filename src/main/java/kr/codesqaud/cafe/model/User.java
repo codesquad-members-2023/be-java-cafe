4a44ed06b2d7b2fe8b1d114ca5a88adc3e900c6e
lvalentine6 <ysr1227@naver.com>
2023-03-13 17:48:19 +0900

    Feat : User 클래스 구현

---
+++ src/main/java/kr/codesqaud/cafe/model/User.java
@@ -0,0 +1,37 @@
+package kr.codesqaud.cafe.model;
+
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
