7f56f58d4963aa0b35347cd6f9468a9a35c38e6c
CiRe <dltpwns6@naver.com>
2023-03-13 15:50:27 +0900

    feat : 회원 가입과 조회를 위한 로직을 간단하게 구현하였다.
    사용자가 회원가입을 하기위한 기능과, 조회를 하기위한 기능을 추가하였다.

--- src/main/java/kr/codesqaud/cafe/controller/UserLogicController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserLogicController.java
@@ -1,18 +1,59 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.basic.User;
+import kr.codesqaud.cafe.repository.UserMemoryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/users")
 public class UserLogicController {
+    UserMemoryRepository userMemoryRepository;
 
+    @Autowired
+    public UserLogicController(UserMemoryRepository userMemoryRepository) {
+        this.userMemoryRepository = userMemoryRepository;
+    }
 
-
+    @ResponseBody
     @GetMapping("/create")
-    public void create(){
+    public String create(@RequestParam String userId,
+                       @RequestParam String password,
+                       @RequestParam String name,
+                       @RequestParam String email
+                       ){
+        userMemoryRepository.join(new User(userId, password, name, email));
 
+        return "create";
+    }
+
+    @ResponseBody
+    @GetMapping
+    public String list() {
+        List<User> users = userMemoryRepository.findAll();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (User user : users) {
+            stringBuilder.append(user.getUserId()).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public String profile(@PathVariable String userId) {
+        User user = userMemoryRepository.findUser(userId);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(user.getUserId()).append("\n");
+        stringBuilder.append(user.getPassword()).append("\n");
+        stringBuilder.append(user.getName()).append("\n");
+        stringBuilder.append(user.getEmail()).append("\n");
+
+        return stringBuilder.toString();
     }
 
 
