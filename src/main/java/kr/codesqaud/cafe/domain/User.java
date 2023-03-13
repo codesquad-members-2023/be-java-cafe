28fbfd7ca9cedc0fb79f9f01bab436713fad7fa9
saehim <saeilee.vxv@gmail.com>
2023-03-13 17:02:41 +0900

    Feat (User) : `User` 도메인 생성
    - 회원가입 폼을 참고해 멤버 변수 설정

--- src/main/java/kr/codesqaud/cafe/domain/User.java
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -7,6 +7,18 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter @Builder
-public class Member {
-    
+public class User {
+    private long id;
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(long id, String userId, String password, String name, String email) {
+        this.id = id;
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
 }
