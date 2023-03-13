b17cad0391bde7bf4106356a57c1253874da5d44
CiRe <dltpwns6@naver.com>
2023-03-13 15:52:46 +0900

    feat : 사용자에 대한 정보를 저장하기 위한 저장소 클래스를 구현하였다.
    User 클래스를 저장하기 위한 저장소 클래스를 구현하였다.

--- src/main/java/kr/codesqaud/cafe/repository/UserMemoryRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/UserMemoryRepository.java
@@ -1,4 +1,38 @@
 package kr.codesqaud.cafe.repository;
 
+import kr.codesqaud.cafe.basic.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@Repository
 public class UserMemoryRepository {
+    private final Map<String, User> userRepository;
+
+    @Autowired
+    public UserMemoryRepository() {
+        this.userRepository = new HashMap<>();
+    }
+
+    public void join(User user) {
+        String userId = user.getUserId();
+        userRepository.put(userId, user);
+    }
+
+    public User findUser(String userId) {
+        return userRepository.get(userId);
+    }
+
+    public List<User> findAll() {
+        return new ArrayList<>(userRepository.values());
+    }
+
 }
