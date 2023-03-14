8968c54db352a69f1f0123ecc9797f1e8eda5938
saehim <saeilee.vxv@gmail.com>
2023-03-14 14:45:13 +0900

    Feat (dto) : `UserRequestDto` 생성
    - 회원가입 시 입력받는 값 DTO 생성

--- src/main/java/kr/codesqaud/cafe/dto/UserJoinRequestDto.java
+++ src/main/java/kr/codesqaud/cafe/dto/UserJoinRequestDto.java
@@ -1,2 +1,30 @@
-package kr.codesqaud.cafe.dto;public class UserJoinRequestDto {
+package kr.codesqaud.cafe.dto;
+
+import kr.codesqaud.cafe.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+@NoArgsConstructor
+public class UserJoinRequestDto {
+    private String username;
+    private String password;
+    private String passwordConfirm;
+    private String email;
+    private String phone;
+
+    public User toEntity() {
+        if (password!=passwordConfirm) return null;
+
+        return User.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .phone(phone)
+                .localDate(LocalDate.now())
+                .build();
+    }
 }
