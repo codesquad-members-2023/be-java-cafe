0ddea8466290af3cf098104510db79218deaa5ce
saehim <saeilee.vxv@gmail.com>
2023-03-14 17:11:31 +0900

    Feat (CafeController) : index view method
    - 동적으로 변경

--- src/main/java/kr/codesqaud/cafe/controller/CafeController.java
+++ src/main/java/kr/codesqaud/cafe/controller/CafeController.java
@@ -1,2 +1,13 @@
-package kr.codesqaud.cafe.controller;public class CafeController {
+package kr.codesqaud.cafe.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CafeController {
+    @GetMapping("/")
+    public String viewIndex() {
+        return "index";
+    }
 }
