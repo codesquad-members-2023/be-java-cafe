05fcfefc0f5d8b43800926f011b1ac8c1d97e623
saehim <saeilee.vxv@gmail.com>
2023-03-14 20:24:14 +0900

    Update (RequestDto) : 회원 코드를 생성하는 로직 수정
    - 중복 `userId` 방지를 위해 1씩 커지도록 수정

--- src/main/java/kr/codesqaud/cafe/dto/UserJoinRequestDto.java
+++ src/main/java/kr/codesqaud/cafe/dto/UserJoinRequestDto.java
@@ -13,18 +13,15 @@ public class UserJoinRequestDto {
     private String email;
     private String nickname;
     private String password;
+    private Long sequence = 0L;
 
     public User toEntity() {
         return User.builder()
-                .userId(generateUserId())
+                .userId(++ sequence)
                 .email(email)
                 .nickname(nickname)
                 .password(password)
                 .createdDate(LocalDate.now())
                 .build();
-    }
-
-    private Long generateUserId() {
-        return (long) LocalDate.now().hashCode();
     }
 }
