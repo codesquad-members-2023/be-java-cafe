95de222832cd5bc78e13efc8955413cae7357acc
ngw7617@naver.com <ngw7617@naver.com>
2023-03-14 14:52:27 +0900

    feat:요구사항에 따라 회원가입 http 전송 메서드 get->post요청으로 변경
    - Spring configuration 설정 클래스 추가하여 의존성 설정 예정

---
+++ src/main/java/kr/codesqaud/cafe/AutoAppConfig.java
@@ -0,0 +1,10 @@
+package kr.codesqaud.cafe;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import kr.codesqaud.cafe.domain.UserRepository;
+
+@Configuration
+public class AutoAppConfig {
+}
