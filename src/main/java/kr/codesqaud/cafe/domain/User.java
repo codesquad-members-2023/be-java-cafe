8b84f9661eb0dd6f681487926acf59d00481b3c1
JeonHyoChang <jhc1426@naver.com>
2023-03-14 09:57:26 +0900

    ✨feat: User 도메인 생성

--- src/main/java/kr/codesqaud/cafe/domain/User.java
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -1,18 +1,17 @@
 package kr.codesqaud.cafe.domain;
 
 public class User {
-
-    private Long id;
+    private String userId;
     private String password;
     private String name;
     private String email;
 
-    public Long getId() {
-        return id;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getPassword() {
