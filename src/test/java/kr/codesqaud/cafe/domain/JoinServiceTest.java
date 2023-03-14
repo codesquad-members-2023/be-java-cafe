c69423e707405b5e7d8524aea0e0b09ec3791bb8
ngw7617@naver.com <ngw7617@naver.com>
2023-03-14 21:13:24 +0900

    test(step-1):회원 조회 서비스 테스트 코드 작성
    - 회원 등록하는 joinUser와 회원을 ID로 조회하는 lookupUser 메서드 테스트 코드 구현

--- src/test/java/kr/codesqaud/cafe/domain/JoinServiceTest.java
+++ src/test/java/kr/codesqaud/cafe/domain/JoinServiceTest.java
@@ -1,16 +1,33 @@
 package kr.codesqaud.cafe.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.codesqaud.cafe.user.User;
 
 class JoinServiceTest {
+    private JoinService joinService = new JoinServiceImpl(new MemoryUserRepository());
 
+    @Transactional
     @Test
+    @DisplayName("새로운 User 객체를 인자로 받아 회원 Repository에 등록합니다.")
     void join() {
+        //when
+        joinService.join(new User("honux","asd","jeong","ho@naver.com"));
+        //then
+        assertThat(joinService.lookupUser("honux").get().getName()).isEqualTo("jeong");
     }
 
+    @Transactional
     @Test
+    @DisplayName("UserId가 일치하는 회원을 Repository에서 조회합니다.")
     void lookupUser() {
+        //given
+        joinService.join(new User("conux","asd","J","ho@naver.com"));
+        //then
+        assertThat(joinService.lookupUser("conux").get().getName()).isEqualTo("J");
     }
 }
