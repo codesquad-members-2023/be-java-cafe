b22a91c2e507dec5e0e70559f2e9cbb73ff1f133
saehim <saeilee.vxv@gmail.com>
2023-03-14 21:17:46 +0900

    Fix (UserJoinRequestDto) : 회원코드 생성 로직 변경
    - 회원코드가 중복되어 생성되는 에러가 있어 로직 변경

--- src/main/java/kr/codesqaud/cafe/dto/UserJoinRequestDto.java
+++ src/main/java/kr/codesqaud/cafe/dto/UserJoinRequestDto.java
@@ -13,15 +13,19 @@ public class UserJoinRequestDto {
     private String email;
     private String nickname;
     private String password;
-    private Long sequence = 0L;
+    private static Long sequence = 0L;
 
     public User toEntity() {
         return User.builder()
-                .userId(++ sequence)
+                .userId(idIncrease())
                 .email(email)
                 .nickname(nickname)
                 .password(password)
                 .createdDate(LocalDate.now())
                 .build();
+    }
+
+    private static Long idIncrease() {
+        return LocalDate.now().hashCode() + ++sequence;
     }
 }
