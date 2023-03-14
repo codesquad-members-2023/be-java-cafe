697fb6c98edb4560b94ec1625acba432e1c22ddc
leegyeongwhan <leekhy02@naver.com>
2023-03-14 20:40:11 +0900

    feat: 기존의 리포지 토리를 서비스와 리포지토리로 나눴습니다.
    기능을 구현하다보니 자료구조 List와 회원가입 하는 로직을 분리 해야겠다는 생각이들어 둘을 분리했습니다.

---
+++ src/main/java/kr/codesqaud/cafe/cafeservice/domain/repository/MemberRepository.java
@@ -0,0 +1,28 @@
+package kr.codesqaud.cafe.cafeservice.domain.repository;
+
+import kr.codesqaud.cafe.cafeservice.domain.member.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Repository
+public class MemberRepository {
+    final List<Member> store = new ArrayList<>();
+
+    public void save(Member member) {
+        store.add(member);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(Collections.unmodifiableList(store));
+    }
+
+    public Optional<Member> findById(Long userId) {
+        return store.stream().filter(member -> member.getId() == userId).findFirst();
+    }
+
+    public List<Member> findByName(String userName) {
+        return store.stream().filter(member -> member.getUserName() == userName).collect(Collectors.toList());
+    }
+}
