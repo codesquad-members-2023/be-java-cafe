3dbbc848a35d8493043af09e468fcff195bf19b5
leegyeongwhan <leekhy02@naver.com>
2023-03-14 21:18:15 +0900

    feat: 아이디로 하나의 프로필 조회하는 메서드 추가
    아이디를 활용하여 하나의 프로필만 가져오는 메서드를 추가햇습니다

--- src/main/java/kr/codesqaud/cafe/cafeservice/web/basic/AddMemberController.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/web/basic/AddMemberController.java
@@ -2,16 +2,16 @@ package kr.codesqaud.cafe.cafeservice.web.basic;
 
 import kr.codesqaud.cafe.cafeservice.domain.dto.MemberAddDtoRequest;
 import kr.codesqaud.cafe.cafeservice.domain.member.Member;
-import kr.codesqaud.cafe.cafeservice.domain.member.MemberService;
+import kr.codesqaud.cafe.cafeservice.domain.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
@@ -36,28 +36,29 @@ public class AddMemberController {
         return "user/form";
     }
 
-    @PostConstruct
-    void init() {
-        service.save(new Member(1L, "1234", "황현", "ghkdgus29@naver.com"));
-        service.save(new Member(2L, "4321", "황윤", "ghkddbs28@naver.com"));
-    }
-
     @PostMapping("/users")
     public String addMember(@ModelAttribute MemberAddDtoRequest member) {
-        service.save(member.toEntity());
-        log.info(" memberId={}", member.getEmail());
-        log.info(" memberId={}", member.getPassword());
-        log.info(" memberId={}", member.getPassword());
+        service.join(member.toEntity());
+        log.info(" memberEmail={}", member.getEmail());
+        log.info(" memberpwd={}", member.getPassword());
+        log.info(" memberid={}", member.getId());
         return "redirect:/users";
     }
 
     @GetMapping("/users")
     public String memberList(Model model) {
-        List<Member> members = service.findAll();
+        List<Member> members = service.findMembers();
         model.addAttribute("users", members);
         log.info(" member={}", members.get(0).getId());
         log.info("model={}", model);
         return "user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String findByProfile(@PathVariable Long userId, Model model) {
+        Member member = service.findOne(userId);
+        model.addAttribute("member", member);
+        return "user/profile";
     }
 }
 
