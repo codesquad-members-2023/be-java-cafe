b73c2a252e4176b72a0023e3d4468a17f5912d06
leegyeongwhan <leekhy02@naver.com>
2023-03-14 19:13:29 +0900

    feat: addMember DtoMember로 변경
    엔티티를 직접 노출하는건 위험하다고 알고있어 Dto에 대해 학습해보고 DTo를 사용했습니다.

--- src/main/java/kr/codesqaud/cafe/cafeservice/web/basic/AddMemberController.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/web/basic/AddMemberController.java
@@ -1,7 +1,8 @@
 package kr.codesqaud.cafe.cafeservice.web.basic;
 
+import kr.codesqaud.cafe.cafeservice.domain.dto.MemberAddDtoRequest;
 import kr.codesqaud.cafe.cafeservice.domain.member.Member;
-import kr.codesqaud.cafe.cafeservice.domain.member.MemberRepository;
+import kr.codesqaud.cafe.cafeservice.domain.member.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,17 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
 @Slf4j
 public class AddMemberController {
 
-    @Autowired
-    private final MemberRepository memberRepository;
+    private final MemberService service;
 
+    @Autowired
     public AddMemberController() {
-        memberRepository = new MemberRepository();
+        service = new MemberService();
     }
 
     @GetMapping("/")
@@ -34,18 +36,26 @@ public class AddMemberController {
         return "user/form";
     }
 
+    @PostConstruct
+    void init() {
+        service.save(new Member(1L, "1234", "황현", "ghkdgus29@naver.com"));
+        service.save(new Member(2L, "4321", "황윤", "ghkddbs28@naver.com"));
+    }
+
     @PostMapping("/users")
-    public String addMember(@ModelAttribute Member member) {
-        memberRepository.save(member);
-        log.info(" member={}", member);
+    public String addMember(@ModelAttribute MemberAddDtoRequest member) {
+        service.save(member.toEntity());
+        log.info(" memberId={}", member.getEmail());
+        log.info(" memberId={}", member.getPassword());
+        log.info(" memberId={}", member.getPassword());
         return "redirect:/users";
     }
 
     @GetMapping("/users")
     public String memberList(Model model) {
-        List<Member> members = memberRepository.findAll();
+        List<Member> members = service.findAll();
         model.addAttribute("users", members);
-        log.info("members={}", members);
+        log.info(" member={}", members.get(0).getId());
         log.info("model={}", model);
         return "user/list";
     }
