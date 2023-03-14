dc927f2afac51668d1876601503033af4be6ef70
birdieHyun <flaehdan@gmail.com>
2023-03-14 16:48:01 +0900

    Refactor : 모든 회원 조회하는 메서드 추상화

--- src/main/java/kr/codesqaud/cafe/repository/SignUpService.java
+++ src/main/java/kr/codesqaud/cafe/repository/SignUpService.java
@@ -2,8 +2,12 @@ package kr.codesqaud.cafe.repository;
 
 import kr.codesqaud.cafe.user.User;
 
+import java.util.List;
+
 public interface SignUpService {
     void join(User user);
 
     User findById(String userId);
+
+    List<User> findAll();
 }
