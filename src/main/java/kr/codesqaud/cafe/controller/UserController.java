7b42659f8d8849c65c0447bfe59cc39e7dd3e37e
birdieHyun <flaehdan@gmail.com>
2023-03-14 19:53:43 +0900

    Feat : 회원 목록 전달하는 메서드 추가
    redirect를 통해 회원 목록 조회

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -25,19 +25,21 @@ public class UserController {
 
     // 회원 가입
     @PostMapping("/user/create")
-    public String addUser(@RequestParam String userId,
-                          @RequestParam String password,
-                          @RequestParam String name,
-                          @RequestParam String email,
-                          Model model) {
+    public String addUser(@ModelAttribute User user) {
 
         log.info("addUser 호출");
 
-        User user = new User(userId, password, name, email);
         signUpService.join(user);
-        model.addAttribute("user", user);
 
         return "redirect:/user/list";
     }
 
+    @GetMapping("/user/list")
+    public String userList(Model model) {
+        log.info("userList 실행");
+        List<User> userList = signUpService.findAll();
+        model.addAttribute("userList", userList);
+
+        return "/user/list";
+    }
 }
