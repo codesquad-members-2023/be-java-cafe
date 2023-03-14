6d260263e779d8706bc9e3b619ae591e21e0cbf5
birdieHyun <flaehdan@gmail.com>
2023-03-14 15:36:06 +0900

    Feat : User 클래스 추가
    회원가입 할 때 입력하는 정보를 멤버변수로 가지는 클래스 생성

---
+++ src/main/java/kr/codesqaud/cafe/user/User.java
@@ -0,0 +1,31 @@
+package kr.codesqaud.cafe.user;
+
+public class User {
+    private final String id;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public User(String id, String password, String name, String email) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
