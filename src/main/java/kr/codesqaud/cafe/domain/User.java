c24ceea6852e7593017c89475ae3e3e85fd5304d
saehim <saeilee.vxv@gmail.com>
2023-03-14 17:04:09 +0900

    Update (User) : entity 멤버 수정
    - 사용하지 않는 멤버 삭제

--- src/main/java/kr/codesqaud/cafe/domain/User.java
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -1,27 +1,23 @@
 package kr.codesqaud.cafe.domain;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter @Builder
 public class User {
     private Long userId;
-    private String username;
-    private String password;
     private String email;
-    private String phone;
-    private LocalDate localDate;
+    private String nickname;
+    private String password;
+    private LocalDate createdDate;
 
-    public User(Long userId, String username, String password, String email, String phone, LocalDate localDate) {
+    public User(Long userId, String email, String nickname, String password, LocalDate localDate) {
         this.userId = userId;
-        this.username = username;
-        this.password = password;
         this.email = email;
-        this.phone = phone;
-        this.localDate = localDate;
+        this.nickname = nickname;
+        this.password = password;
+        this.createdDate = localDate;
     }
 }
