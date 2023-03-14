69deb5fe651c423aef525ff7caeaea65e070bb19
leegyeongwhan <leekhy02@naver.com>
2023-03-14 19:14:10 +0900

    feat: 어노테이션 삭제
    필요없다 생각되어 생성자 관련 어노테이션을 삭제합니다

--- src/main/java/kr/codesqaud/cafe/cafeservice/domain/member/Member.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/domain/member/Member.java
@@ -1,24 +1,29 @@
 package kr.codesqaud.cafe.cafeservice.domain.member;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
+@AllArgsConstructor
 public class Member {
 
     private Long id;
     private String userName;
     private String password;
     private String email;
-    private String phone;
+    private static long sequence = 0L; //static 사용
 
     public Member() {
     }
 
-    public Member(String userName, String password, String email, String phone) {
+    public Member(String userName, String password, String email) {
+        this.id = idIncrease();
         this.userName = userName;
         this.password = password;
         this.email = email;
-        this.phone = phone;
+    }
+
+    private Long idIncrease() {
+        return ++sequence;
     }
 }
