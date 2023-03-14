3641cf62013a5df906282c6091a46666676d2f2d
saehim <saeilee.vxv@gmail.com>
2023-03-14 19:20:28 +0900

    Feat (User) : `userId`를 비교해주는 메서드 구현
    - isIdEquals 구현

--- src/main/java/kr/codesqaud/cafe/domain/User.java
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -7,17 +7,21 @@ import java.time.LocalDate;
 
 @Getter @Builder
 public class User {
-    private Long userId;
+    private long userId;
     private String email;
     private String nickname;
     private String password;
     private LocalDate createdDate;
 
-    public User(Long userId, String email, String nickname, String password, LocalDate localDate) {
+    public User(long userId, String email, String nickname, String password, LocalDate localDate) {
         this.userId = userId;
         this.email = email;
         this.nickname = nickname;
         this.password = password;
         this.createdDate = localDate;
+    }
+
+    public boolean isIdEquals(long userId) {
+        return this.userId == userId;
     }
 }
