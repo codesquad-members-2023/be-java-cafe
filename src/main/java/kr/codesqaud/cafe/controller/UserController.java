eb0eaa18e980fee87ec408dc742cce330d5761b3
JeonHyoChang <jhc1426@naver.com>
2023-03-15 15:41:36 +0900

    ♻️refactor: HTML 중복 제거(template에 들어간것만 중복제거)

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -2,6 +2,8 @@ package kr.codesqaud.cafe.controller;
 
 import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.repository.MemoryUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,8 @@ import java.util.Optional;
 
 @Controller
 public class UserController {
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
     private final MemoryUserRepository repository;
 
     @Autowired
@@ -22,31 +26,40 @@ public class UserController {
         this.repository = repository;
     }
 
+    // form.html Mapping
     @GetMapping("/user/form.html")
-    public String showForm(){
+    public String showForm() {
+        log.info("이것은 Form 인가?!");
+
         return "user/form";
     }
 
+    // 회원가입 POST
     @PostMapping("/users")
     public String signUp(@ModelAttribute User user) {
-        //System.out.println("보냈는가?");
+        log.info("회원가입 POST: 보내졌는가?");
+
         repository.save(user);
         return "redirect:users";
     }
 
+    // 사용자 목록 Mapping
     @GetMapping("/users")
     public String showUsers(Model model) {
+        log.info("사용자 목록 Mapping: 받았는가?");
+
         List<User> users = repository.findAll();
-        //System.out.println("받았는가?");
         model.addAttribute("users", users);
 
         return "user/list";
     }
 
+    // 프로필 Mapping
     @GetMapping("/users/{userId}")
     public String showUserProfile(Model model, @PathVariable String userId) {
-        Optional<User> profile = repository.findById(userId);
+        log.info("프로필 Mapping: 프로필 보러왔는감?");
 
+        Optional<User> profile = repository.findById(userId);
         model.addAttribute("profile", profile.get());
         return "user/profile";
     }
