c6735306344c544153f4efc88cd21721ceff14e9
saehim <saeilee.vxv@gmail.com>
2023-03-15 20:33:11 +0900

    Feat (ArticleRepository) : 글 저장 자료구조 구현
    - 기본 메서드 구현

--- src/main/java/kr/codesqaud/cafe/repository/ArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/ArticleRepository.java
@@ -4,9 +4,23 @@ import kr.codesqaud.cafe.domain.Article;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ArticleRepository {
     List<Article> store = new ArrayList<>();
+
+    public void save(Article article) {
+        store.add(article);
+    }
+
+    public Optional<Article> findById(Long id) {
+        return store.stream().filter(e -> e.isIdEquals(id)).findFirst();
+    }
+
+    public List<Article> findAll() {
+        return new ArrayList<>(Collections.unmodifiableList(store));
+    }
 }
