3f99950fae68fde5385b97e6e05afdd670d9ef6b
ngw7617@naver.com <ngw7617@naver.com>
2023-03-15 16:02:41 +0900

    feat(step-1):회원 프로필 조회 기능 구현
    - 회원 목록에서 id 클릭 시 회원 프로필으로 매핑되도록 기능 구현 완료

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,6 +25,12 @@ public class UserController {
         this.joinService = joinService;
     }
 
+    @GetMapping(value = "/users/{id}")
+    public String userList(Model model, @PathVariable String id) {
+        model.addAttribute("user", joinService.lookupUser(id).get());
+
+        return "/user/profile";
+    }
     @GetMapping("/user/list")
     public String userList(Model model) {
         model.addAttribute("user", joinService.lookupAllUser());
