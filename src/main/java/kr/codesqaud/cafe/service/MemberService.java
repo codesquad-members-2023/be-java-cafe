9d136a0f87bd3cb6349b0a63e230ce02afcba5fa
kimdongmin <kimdongmin@kimdongminui-MacBookPro.local>
2023-03-15 20:28:57 +0900

    feat(mission1): MemberService 클래스 생성
    회원 가입, 회원 검색, 회원 목록 보여주는 기능을 가진 MemberService 클래스 생성

---
+++ src/main/java/kr/codesqaud/cafe/service/MemberService.java
@@ -0,0 +1,36 @@
+package kr.codesqaud.cafe.service;
+
+import kr.codesqaud.cafe.domain.Member;
+import kr.codesqaud.cafe.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.saveMember(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findOneMemberbyEmail(member.getEmail())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOneMember(String userEmail) {
+        return memberRepository.findOneMemberbyEmail(userEmail);
+    }
+}
