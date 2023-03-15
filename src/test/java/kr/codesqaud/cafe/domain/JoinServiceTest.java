e3ac478722e8df2f85c80a91c610a8471243f538
ngw7617@naver.com <ngw7617@naver.com>
2023-03-15 12:16:57 +0900

    feat:정적 파일 template으로 경로 변경 및 header mustache로 중복 제거
    - nav 및 footer 중복 제거 예정
    - 회원가입 Service 및 Repository Test 코드 불필요한 annotation 제거 및 리팩터링

--- src/test/java/kr/codesqaud/cafe/domain/JoinServiceTest.java
+++ src/test/java/kr/codesqaud/cafe/domain/JoinServiceTest.java
@@ -4,14 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.transaction.annotation.Transactional;
 
 import kr.codesqaud.cafe.user.User;
 
 class JoinServiceTest {
     private JoinService joinService = new JoinServiceImpl(new MemoryUserRepository());
 
-    @Transactional
     @Test
     @DisplayName("새로운 User 객체를 인자로 받아 회원 Repository에 등록합니다.")
     void join() {
@@ -21,7 +19,6 @@ class JoinServiceTest {
         assertThat(joinService.lookupUser("honux").get().getName()).isEqualTo("jeong");
     }
 
-    @Transactional
     @Test
     @DisplayName("UserId가 일치하는 회원을 Repository에서 조회합니다.")
     void lookupUser() {
