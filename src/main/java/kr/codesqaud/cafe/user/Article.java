c88b5c6a6f3e20a767e1c4742992ce14236c8ced
ngw7617@naver.com <ngw7617@naver.com>
2023-03-15 20:33:23 +0900

    feat(step-2):글의 글쓴이, 제목, 내용을 포함하는 Article 타입 구현
    - 질문 글쓰기 서비스인 QnaService에서 사용할 글 타입 객체 구현

---
+++ src/main/java/kr/codesqaud/cafe/user/Article.java
@@ -0,0 +1,25 @@
+package kr.codesqaud.cafe.model;
+
+public class Article {
+    private String writer;
+    private String title;
+    private String contents;
+
+    public Article(String writer, String title, String contents) {
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+}
