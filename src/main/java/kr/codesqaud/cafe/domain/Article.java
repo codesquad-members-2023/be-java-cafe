1beca221724e30fe9543a39dd7f6e11071582f49
saehim <saeilee.vxv@gmail.com>
2023-03-15 20:39:09 +0900

    Feat (Article) : 멤버 수정 / id 생성자

--- src/main/java/kr/codesqaud/cafe/domain/Article.java
+++ src/main/java/kr/codesqaud/cafe/domain/Article.java
@@ -1,11 +1,17 @@
 package kr.codesqaud.cafe.domain;
 
-public class Article {
-    private User writer;
-    private String title;
-    private String contents;
+import java.time.LocalDate;
+import java.util.Objects;
 
-    public User getWriter() {
+public class Article {
+    private final Long id;
+    private final String writer;
+    private final String title;
+    private final String contents;
+    private LocalDate createdDate;
+    private static Long sequence = 0L;
+
+    public String getWriter() {
         return writer;
     }
 
@@ -17,9 +23,19 @@ public class Article {
         return contents;
     }
 
-    public Article(User writer, String title, String contents) {
+    public Article(String writer, String title, String contents) {
+        this.id = generateId();
         this.writer = writer;
         this.title = title;
         this.contents = contents;
+        this.createdDate = LocalDate.now();
+    }
+
+    public boolean isIdEquals(Long articleId) {
+        return this.id.equals(articleId);
+    }
+
+    private static Long generateId() {
+        return ++ sequence;
     }
 }
