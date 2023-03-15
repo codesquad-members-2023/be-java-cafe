95d7aab0d375506c3ee3e8907bf457bc6bf4738a
saehim <saeilee.vxv@gmail.com>
2023-03-15 20:40:55 +0900

    Feat (ArticleContorller) : 질문 작성 url 매핑
    - 의존성 주입 방식 변경
    - 질문 글 작성 매핑

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -1,19 +1,40 @@
 package kr.codesqaud.cafe.controller;
 
 import kr.codesqaud.cafe.domain.Article;
+import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.repository.ArticleRepository;
+import kr.codesqaud.cafe.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Controller
 public class ArticleController {
-    @Autowired
-    ArticleRepository articleRepository;
+
+    private final ArticleRepository articleRepository;
+
+    Logger logger = LoggerFactory.getLogger(ArticleController.class);
+
+    public ArticleController(ArticleRepository articleRepository, UserRepository userRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     @PostMapping("/questions")
-    public String postQuestions(Article article) {
+    public String postQuestions(String writer, String title, String contents) {
+        Article article = new Article(writer, title, contents);
+        logger.info(article.getWriter());
         articleRepository.save(article);
+        return "redirect:/";
+    }
     }
 }
