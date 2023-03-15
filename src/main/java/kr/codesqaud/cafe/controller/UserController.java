019cab37f66c152e8b62064b0a5cd7faa767a8ab
lvalentine6 <ysr1227@naver.com>
2023-03-16 01:48:03 +0900

    Refactor : 메서드명 변경 및 findUserProfile 메서드 구조 변경

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -1,21 +1,15 @@
 package kr.codesqaud.cafe.controller;
 
-import java.util.List;
 import kr.codesqaud.cafe.model.User;
 import kr.codesqaud.cafe.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -36,14 +30,14 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public String findUser(Model model) {
+    public String findUserList(Model model) {
         model.addAttribute("userList", userService.findUserAll());
         return "list2";
     }
 
     @GetMapping("users/{userId}")
-    public String getProfile(@PathVariable("userId") String userId, Model model) {
-        model.addAttribute(userId);
+    public String findUserProfile(@PathVariable("userId") String userId, Model model) {
+        model.addAttribute("userList", userService.findUserByUserId(userId));
         return "profile2";
     }
 }
