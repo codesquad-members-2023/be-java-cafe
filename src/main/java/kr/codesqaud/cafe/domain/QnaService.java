46155b5b62b15b1f3c54c6928594deb85ff8e3b4
ngw7617@naver.com <ngw7617@naver.com>
2023-03-15 20:38:14 +0900

    feat(step-2):질문 글쓰기 서비스 QnaService 인터페이스를 정의하고 구현체 QnaServiceImpl 구현
    - 메서드는 postQna 및 필요에 따라 추가 예정

---
+++ src/main/java/kr/codesqaud/cafe/domain/QnaService.java
@@ -0,0 +1,6 @@
+package kr.codesqaud.cafe.domain;
+
+public interface QnaService {
+    void postQna();
+
+}
---
+++ src/main/java/kr/codesqaud/cafe/domain/QnaServiceImpl.java
@@ -0,0 +1,11 @@
+package kr.codesqaud.cafe.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QnaServiceImpl implements QnaService {
+    @Override
+    public void postQna() {
+
+    }
+}
