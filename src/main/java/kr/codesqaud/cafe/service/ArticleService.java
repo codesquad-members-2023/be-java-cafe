dec6b51684187c21efc67d51634e7d711dba88b2
lvalentine6 <ysr1227@naver.com>
2023-03-16 22:06:37 +0900

    Feat : ArticleService 클래스 구현

---
+++ src/main/java/kr/codesqaud/cafe/service/ArticleService.java
@@ -0,0 +1,30 @@
+package kr.codesqaud.cafe.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import kr.codesqaud.cafe.model.Article;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+    private final List<Article> articleList;
+
+    public ArticleService(List<Article> articleList) {
+        this.articleList = articleList;
+    }
+
+    public void creatArticle(Article article) {
+        article.giveId(articleList.size() + 1);
+        articleList.add(article);
+    }
+
+    public List<Article> findAllArticle() {
+        return articleList;
+    }
+
+    public List<Article> findArticleContentById(int articleId) {
+        return articleList.stream()
+                .filter(s -> s.getId() == articleId)
+                .collect(Collectors.toList());
+    }
+}
