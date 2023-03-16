ec4468b88b25903e0a3915dfa13ac3e8ac5eb681
birdieHyun <flaehdan@gmail.com>
2023-03-16 09:29:40 +0900

    Refactor : 불필요한 인터페이스 제거
    서비스 클래스의 인터페이스 삭제

--- src/test/java/kr/codesqaud/cafe/domain/repository/UserSaveTest.java
+++ src/test/java/kr/codesqaud/cafe/domain/repository/UserSaveTest.java
@@ -2,19 +2,16 @@ package kr.codesqaud.cafe.domain.repository;
 
 import kr.codesqaud.cafe.repository.MemoryMemberRepository;
 import kr.codesqaud.cafe.repository.SignUpService;
-import kr.codesqaud.cafe.repository.SignUpServiceImpl;
 import kr.codesqaud.cafe.user.User;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
 class UserSaveTest {
 
-    private SignUpService signUpService = new SignUpServiceImpl(new MemoryMemberRepository());
+    private SignUpService signUpService = new SignUpService(new MemoryMemberRepository());
 
     @Test
     @DisplayName("회원 가입 잘 되는지 테스트")
