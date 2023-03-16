5927aab5c873b19b1554fccba44285551903fb84
Hyun <ghkdgus29@naver.com>
2023-03-16 13:58:42 +0900

    feat: 홈으로 redirect 시 article 저장소에서 모든 글 목록들을 불러와 출력
    타임리프를 사용하여 글 목록들을 출력

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -6,8 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
@@ -18,6 +22,14 @@ public class ArticleController {
     @Autowired
     public ArticleController(MemoryArticleRepository repository) {
         this.repository = repository;
+    }
+
+    @GetMapping("/")
+    public String showArticles(Model model) {
+        List<Article> articles = repository.findAll();
+        model.addAttribute("articles", articles);
+
+        return "index";
     }
 
     @PostMapping("/questions")
