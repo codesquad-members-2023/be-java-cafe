af183d7e2fd65ebc5f5c8699f1fc4f9b3ec3392e
Hyun <ghkdgus29@naver.com>
2023-03-16 14:01:50 +0900

    feat: article 목록 초기데이터 저장
    눈으로 확인하기 위해 2개의 글을 미리 저장

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
@@ -24,6 +25,16 @@ public class ArticleController {
         this.repository = repository;
     }
 
+
+    @PostConstruct
+    void init() {
+        Article article1 = new Article("Hyun", "이거 실화냐?", "미안하다. 이거보여주려고 어그로 끌었다.");
+        Article article2 = new Article("Yoon", "엥?", "미안하다. 이거보여주려고 어그로 또 끌었다.");
+
+        repository.save(article1);
+        repository.save(article2);
+    }
+
     @GetMapping("/")
     public String showArticles(Model model) {
         List<Article> articles = repository.findAll();
@@ -35,11 +46,6 @@ public class ArticleController {
     @PostMapping("/questions")
     public String question(@ModelAttribute Article article) {
         repository.save(article);
-
-        log.info("{}", article.getId());
-        log.info("{}", article.getWriter());
-        log.info("{}", article.getTitle());
-        log.info("{}", article.getContents());
 
         return "redirect:/";
     }
