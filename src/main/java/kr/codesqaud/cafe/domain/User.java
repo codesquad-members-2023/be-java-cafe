4698058aaff647bfb0743037a8db229eabe7f49f
Life is a ninanino <tvxqinak@naver.com>
2023-03-16 16:15:21 +0900

    refactor : 코드 수정
    User class 에 생성자를 추가해 setter 삭제, UserRepository Class 변수명 변경, html 변수명 변경

--- src/main/java/kr/codesqaud/cafe/domain/User.java
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -7,38 +7,27 @@ public class User {
     private String userName;
     private String email;
 
-    public User() {
+    public User(String userId, String password, String userName, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.userName = userName;
+        this.email = email;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
