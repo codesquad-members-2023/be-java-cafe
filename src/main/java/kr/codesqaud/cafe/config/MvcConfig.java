026026fe49a615c5232c446aeadd33219f581463
jaea-kim <jaeakim98@gmail.com>
2023-03-16 20:11:33 +0900

    기본 resource path을 인식하지 못해 addResourceHandlers로 커스터마이징 및 필요없는 에노테이션 제거

--- src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
+++ src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
@@ -2,18 +2,25 @@ package kr.codesqaud.cafe.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc //WebMvcConfigurationSupport에서 구성한 스프링 MVC 구성을 불러옴
 public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/");
+
+    }
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         registry.addViewController("/user/form").setViewName("user/form");
+        registry.addViewController("/user/login").setViewName("user/login");
+        registry.addViewController("/questions/form").setViewName("qna/form");
     }
 }
 
