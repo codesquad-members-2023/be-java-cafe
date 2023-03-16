8ca956d00da5f30d153729a782c843af5e3a3051
jaea-kim <jaeakim98@gmail.com>
2023-03-16 12:03:13 +0900

    UserForm class 생성
    회원가입할 때 전달한 값을 저장할 수 있는 클래스
    setter와 getter 메소드만 생성

---
+++ src/main/java/kr/codesqaud/cafe/controller/UserForm.java
@@ -0,0 +1,33 @@
+package kr.codesqaud.cafe.controller;
+
+public class UserForm {
+    private String name;
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
