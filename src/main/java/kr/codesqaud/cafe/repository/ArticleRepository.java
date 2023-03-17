9ae041569dcefff8b57dea02994baa43b5de3d81
jaea-kim <jaeakim98@gmail.com>
2023-03-17 10:14:10 +0900

    articleRepository에 매개변수로 받은 article 객체 저장

--- src/main/java/kr/codesqaud/cafe/repository/ArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/ArticleRepository.java
@@ -1,2 +1,17 @@
-package kr.codesqaud.cafe.repository;public class ArticleRepository {
+package kr.codesqaud.cafe.repository;
+
+import kr.codesqaud.cafe.domain.Article;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ArticleRepository {
+    private static final List<Article> articles = new ArrayList<>();
+
+    public void save(Article article) {
+        articles.add(article);
+        System.out.println(article);
+    }
 }
