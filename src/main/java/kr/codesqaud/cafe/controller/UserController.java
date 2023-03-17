e0ff61fe31e1bcb225e4151aa95dd49de1ec5bd4
saehim <saeilee.vxv@gmail.com>
2023-03-17 21:16:25 +0900

    Refactor : `dto` 삭제
    - 사용하지 않는 객체 삭제

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -1,53 +1,50 @@
 package kr.codesqaud.cafe.controller;
 
-import kr.codesqaud.cafe.domain.User;
-import kr.codesqaud.cafe.dto.UserJoinRequestDto;
-import kr.codesqaud.cafe.dto.UserListResponseDto;
-import kr.codesqaud.cafe.service.UserService;
+import kr.codesqaud.cafe.domain.Member;
+import kr.codesqaud.cafe.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Controller
 public class UserController {
 
-    private final UserService userService;
+    private final UserRepository userRepository;
     private Logger LOG = LoggerFactory.getLogger(UserController.class.getName());
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/users")
-    public String addUser(@ModelAttribute UserJoinRequestDto dto) throws IllegalAccessException {
-        userService.join(dto.toEntity());
+    public String addUser(String userId, String email, String nickname, String password) {
+        userRepository.save(new Member(userId, email, nickname, password));
         return "redirect:/users";
     }
 
     @GetMapping("/users")
     public String list(Model model) {
-        List<UserListResponseDto> collect = userService.findAll().stream()
-                .map(UserListResponseDto::new)
-                .collect(Collectors.toList());
+        List<Member> all = userRepository.findAll();
 
-        model.addAttribute("list", collect);
-        model.addAttribute("size", collect.size());
+        model.addAttribute("list", all);
+        model.addAttribute("size", all.size());
         return "user/list";
     }
 
     @GetMapping("/users/{userId}")
     public String profile(@PathVariable Long userId, Model model) {
-        Optional<User> user = userRepository.findById(userId);
+        Optional<Member> user = userRepository.findById(userId);
         try {
-            model.addAttribute("profile", user.orElseThrow());
+            model.addAttribute("profile", user.orElseThrow(() -> new NoSuchElementException("해당하는 회원이 없습니다.")));
         } catch (NoSuchElementException e) {
-            return "user/profile_failed";
+            model.addAttribute("message", e.getMessage());
+            return "layout/error";
         }
         return "user/profile";
     }
