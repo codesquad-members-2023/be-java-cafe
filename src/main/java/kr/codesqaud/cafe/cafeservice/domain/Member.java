e7e44aa1255a39b0845e130e8e58eb7c322ddc3f
leegyeongwhan <leekhy02@naver.com>
2023-03-17 10:00:34 +0900

    feat: update를 위한 setter 추가합니다.
    회원 정보를 수정하기 위해 id값을 입력받고 다른 회원 정보를 수정하기 위해 setter를 추가합니다

--- src/main/java/kr/codesqaud/cafe/cafeservice/domain/Member.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/domain/Member.java
@@ -35,4 +35,16 @@ public class Member {
     public String getEmail() {
         return email;
     }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
