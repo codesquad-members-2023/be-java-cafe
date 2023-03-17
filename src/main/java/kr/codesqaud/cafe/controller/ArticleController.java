ff0afa0403a00d4737597b6f05c418437944f669
jaea-kim <jaeakim98@gmail.com>
2023-03-17 10:11:25 +0900

    /questions로 postMapping하여 articleRepostiory에 받은 데이터 저장

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -1,2 +1,26 @@
-package kr.codesqaud.cafe.controller;public class ArticleController {
+package kr.codesqaud.cafe.controller;
+
+import kr.codesqaud.cafe.domain.Article;
+import kr.codesqaud.cafe.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    private final ArticleRepository articleRepository;
+
+    @Autowired
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @PostMapping("/questions")
+    public String post(ArticleForm form) {
+        Article article = new Article(form.getWriter(), form.getTitle(), form.getContent());
+
+        articleRepository.save(article);
+
+        return "redirect:/";
+    }
 }
