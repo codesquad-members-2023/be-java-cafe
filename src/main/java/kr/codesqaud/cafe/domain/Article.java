f76a2af0e18e41509892f61569efaffb9cdcd43d
kimdongmin <kimdongmin@kimdongminui-MacBookPro.local>
2023-03-17 16:04:59 +0900

    feat(mission1): Article 클래스 생성하기

---
+++ src/main/java/kr/codesqaud/cafe/domain/Article.java
@@ -0,0 +1,37 @@
+package kr.codesqaud.cafe.domain;
+
+public class Article {
+    private String author;
+    private String title;
+    private String content;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Article(String author, String title, String content) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+    }
+}
