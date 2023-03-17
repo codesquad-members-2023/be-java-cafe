58a62c90b152a78b1a6e78f5adf7b7de92c283e6
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-17 14:49:43 +0900

    QnaRepository 구현

--- src/main/java/kr/codesqaud/cafe/repository/QnaRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/QnaRepository.java
@@ -1,10 +1,29 @@
-//package kr.codesqaud.cafe.repository;
-//
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public class QnaRepository {
-//
-//    private static Map<Integer, >
-//
-//}
+package kr.codesqaud.cafe.repository;
+
+import kr.codesqaud.cafe.domain.Qna;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class QnaRepository {
+
+    private static final Map<Integer, Qna> store = new ConcurrentHashMap<>();
+
+    public void save(Qna qna) {
+        store.put(store.size(), qna);
+    }
+
+    public List<Qna> findAllQnas() {
+        return new ArrayList<>(store.values());
+    }
+
+    public Qna findByQnaIndex(Integer index) {
+        Qna qna = store.get(index);
+        return qna;
+    }
+
+}
