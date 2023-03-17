39076310d247053241a798c760c48753ff0bd998
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-17 14:24:47 +0900

    addViewController 적용

---
+++ src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package kr.codesqaud.cafe.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/users/form").setViewName("users/form");
+        registry.addViewController("/users/login").setViewName("users/login");
+        registry.addViewController("/questions/form").setViewName("qna/form");
+    }
+}
+
