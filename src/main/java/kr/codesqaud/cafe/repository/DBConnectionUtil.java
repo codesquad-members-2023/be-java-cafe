919a86c4a5cb19e4de3648fba6cee717c8296b53
birdieHyun <flaehdan@gmail.com>
2023-03-18 10:16:23 +0900

    Feat : DB 연결을 위한 커넥터 클래스 생성

---
+++ src/main/java/kr/codesqaud/cafe/repository/DBConnectionUtil.java
@@ -0,0 +1,28 @@
+package kr.codesqaud.cafe.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnectionUtil {
+
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    public Connection getConnection() {
+        final String URL = "jdbc:h2:tcp://localhost/~/test";
+        final String USERNAME = "sa";
+        final String PASSWORD = "";
+
+        try {
+            Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            log.info(String.valueOf(con.getClass()));
+            return con;
+        } catch (SQLException e) {
+            log.info("con error");
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
