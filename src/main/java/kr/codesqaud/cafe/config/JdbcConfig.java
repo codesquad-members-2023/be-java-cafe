ce4c951b0658ab4d2bf722dbae96700a85ef3ce1
lvalentine6 <ysr1227@naver.com>
2023-03-18 17:05:20 +0900

    Feat : JdbcConfig 추가

---
+++ src/main/java/kr/codesqaud/cafe/config/JdbcConfig.java
@@ -0,0 +1,26 @@
+package kr.codesqaud.cafe.config;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class JdbcConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
