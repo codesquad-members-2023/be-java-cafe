c6a6b71fdf803cd5c0c19d6e2b52cc292fe2309f
saehim <saeilee.vxv@gmail.com>
2023-03-20 00:06:00 +0900

    Feat (repository) : `Member` CRUD DB와 연결
    - JDBC 템플릿을 사용하여 CRUD 구현
    - `Member` 파싱을 위한 `RowMapper` 구현

--- src/main/java/kr/codesqaud/cafe/repository/MemoryUserRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/MemoryUserRepository.java
@@ -1,47 +1,72 @@
 package kr.codesqaud.cafe.repository;
 
 import kr.codesqaud.cafe.domain.Member;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
 public class MemoryUserRepository implements UserRepository {
-    private final Queue<Member> store = new ConcurrentLinkedQueue<>();
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemoryUserRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     @Override
-    public void save(Member user) {
-        if (vaildName(user.getNickname())) {
-            store.add(user);
+    public void save(Member member) {
+        if (vaildMemberId(member.getNickname())) {
+            String sql = "INSERT INTO MEMBER (USERID, NICKNAME, EMAIL, PASSWORD) VALUES (?, ?, ?, ?)";
+            jdbcTemplate.update(sql,
+                    member.getUserId(),
+                    member.getNickname(),
+                    member.getEmail(),
+                    member.getPassword());
         }
     }
 
     @Override
-    public Optional<Member> findById(long userId) {
-        return store.stream().filter(user -> user.equals(userId)).findAny();
+    public Optional<Member> findById(Long userId) {
+        String sql = "SELECT ID, USERID, NICKNAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT FROM MEMBER WHERE ID = ?";
+        return jdbcTemplate.query(sql, new MemberRowMapper(), userId).stream().findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        return List.copyOf(store);
+        String sql = "SELECT ID, USERID, NICKNAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT FROM MEMBER";
+        return jdbcTemplate.query(sql, new MemberRowMapper());
     }
 
     @Override
-    public void clear() {
-        store.clear();
+    public boolean vaildMemberId(String userName) {
+        String sql = "SELECT ID, USERID FROM MEMBER WHERE USERID = ?";
+        return jdbcTemplate.query(sql, new MemberRowMapper(), userName).isEmpty();
     }
 
-    @Override
-    public boolean vaildName(String userName) {
-        return store.stream().noneMatch(e -> e.isNameEquals(userName));
-    }
-    
     @Override
     public void update(Member exMember, Member newUser) throws NoSuchElementException {
-        exMember.update(newUser);
+        Member updatedMember = exMember.update(newUser);
+        String sql = "UPDATE MEMBER SET NICKNAME = ?, EMAIL = ?, PASSWORD = ? WHERE USERID = ?";
+        jdbcTemplate.update(sql, updatedMember.getNickname(), updatedMember.getEmail(), updatedMember.getPassword(), updatedMember.getUserId());
+    }
+
+    private static final class MemberRowMapper implements RowMapper<Member> {
+        public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Member member = new Member();
+            member.setId(rs.getLong("ID"));
+            member.setUserId(rs.getString("USERID"));
+            member.setNickname(rs.getString("NICKNAME"));
+            member.setPassword(rs.getString("PASSWORD"));
+            member.setEmail(rs.getString("EMAIL"));
+            member.setCreatedDate(rs.getTimestamp("CREATED_AT"));
+            member.setUpdatedDate(rs.getTimestamp("UPDATED_AT"));
+            return member;
+        }
     }
 }
