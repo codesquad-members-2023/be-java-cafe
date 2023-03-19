fd8a0fe16a3395c10f9bbada56c9d10e9b1ffc80
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-19 23:05:25 +0900

    Feat: ArticleController.java 기능 추가
    게시글 관련 뷰들과 매핑 처리 기능 구현

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -1,4 +1,47 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.domain.article.Article;
+import kr.codesqaud.cafe.domain.article.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+
+    @Autowired
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @GetMapping("/")
+    public String articles(Model model) {
+        List<Article> articles = articleRepository.showAllArticles();
+        model.addAttribute("articles", articles);
+        return "index";
+    }
+
+    @GetMapping("/qna/questions")
+    public String writeForm() {
+        return "qna/form";
+    }
+
+    @PostMapping("/qna/questions")
+    public String saveQuestion(@ModelAttribute("article") Article article) {
+        articleRepository.write(article);
+        return "redirect:/";
+    }
+
+    @GetMapping("/articles/{articleSequence}")
+    public String articleShow(@PathVariable Long articleSequence, Model model) {
+        Article findArticle = articleRepository.findByArticleSequence(articleSequence);
+        model.addAttribute("article", findArticle);
+        return "qna/show";
+    }
 }
