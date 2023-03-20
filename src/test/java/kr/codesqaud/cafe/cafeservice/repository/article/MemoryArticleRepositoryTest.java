ba60773a4c1023abf9dbf2468ff50cc157890c0f
leegyeongwhan <leekhy02@naver.com>
2023-03-20 16:33:41 +0900

    feat: 로컬에서만 돌아가는 repository추가
    기존 쓰던 repositroy를 db연동하는 repository와 구분하기 위해 따로 만듭니다.

---
+++ src/test/java/kr/codesqaud/cafe/cafeservice/repository/article/MemoryArticleRepositoryTest.java
@@ -0,0 +1,43 @@
+package kr.codesqaud.cafe.cafeservice.repository.article;
+
+import kr.codesqaud.cafe.cafeservice.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemoryArticleRepositoryTest {
+
+    ArticleRepository repository;
+
+    public MemoryArticleRepositoryTest(@Qualifier ArticleRepository articleRepository) {
+        this.repository = articleRepository;
+    }
+
+    @Test
+    @DisplayName("Article 저장")
+    void save() {
+        Article article = new Article("감자", "제목", "내용");
+        repository.save(article);
+        assertEquals(article.getId(), 0);
+    }
+
+    @Test
+    @DisplayName("Article 아이디 탐색")
+    void findById() {
+        Article article = new Article("감자", "제목", "내용");
+        repository.save(article);
+        Optional<Article> byId = repository.findById(0L);
+        assertEquals(article, byId.orElseThrow());
+    }
+}
