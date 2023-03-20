c006a7c7839dfd844f5cb424dff9d7c6165d2ca3
saehim <saeilee.vxv@gmail.com>
2023-03-20 12:36:10 +0900

    Add : `Exception Handler` 구현을 위한 클래스 추가

---
+++ src/main/java/kr/codesqaud/cafe/controller/ExceptionHandler.java
@@ -0,0 +1,9 @@
+package kr.codesqaud.cafe.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice   // 모든 컨트롤러에 적용
+public class ExceptionHandler {
+    // TODO 예외처리 통합 하기
+
+}
