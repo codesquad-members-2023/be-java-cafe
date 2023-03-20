f5fa3cd4d5803482e2170359460067ea8cfac5f7
jaea-kim <jaeakim98@gmail.com>
2023-03-20 17:38:23 +0900

    ArticleRepository 를 db없이 작동하도록 구현

---
+++ src/main/java/kr/codesqaud/cafe/repository/MemoryArticleRepository.java
@@ -0,0 +1,28 @@
+package kr.codesqaud.cafe.repository;
+
+import kr.codesqaud.cafe.domain.Article;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class MemoryArticleRepository implements ArticleRepository{
+    private static final List<Article> articles = new ArrayList<>();
+
+    @Override
+    public void save(Article article) {
+        article.setIndex(articles.size() + 1);
+        articles.add(article);
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return articles;
+    }
+
+    @Override
+    public Article findByIndex(int index) {
+        return articles.get(index - 1);
+    }
+}
