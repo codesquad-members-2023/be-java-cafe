6117c33f16b2473b39a574942ca87c223d800d8d
birdieHyun <flaehdan@gmail.com>
2023-03-20 14:28:21 +0900

    Refactor : 자바빈 규약에 따른 기본생성자 생성

--- src/main/java/kr/codesqaud/cafe/domain/Article.java
+++ src/main/java/kr/codesqaud/cafe/domain/Article.java
@@ -5,11 +5,14 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Article {
-    private final String writer;
-    private final String title;
-    private final String contents;
-    private final Timestamp timestamp;
     private int id;
+    private String writer;
+    private String title;
+    private String contents;
+    private Timestamp timestamp;
+
+    public Article() {
+    }
 
     public Article(String writer, String title, String contents, Timestamp timestamp) {
         this.writer = writer;
@@ -40,5 +43,21 @@ public class Article {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
     }
 }
