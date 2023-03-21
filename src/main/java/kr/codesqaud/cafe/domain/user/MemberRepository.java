458ae75b9bd5140aa08f6d1e52c38f3644105153
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-22 00:27:42 +0900

    Refactor: MemberRepository.java 코드 수정
    User가 SQL의 예약어인 관계로 기존의 User를 Member로 수정함에 따라 발생한 코드 수정

--- src/main/java/kr/codesqaud/cafe/domain/user/MemberRepository.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/MemberRepository.java
@@ -6,31 +6,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class UserRepository {
+public class MemberRepository {
 
-    private static final List<User> userRepository = new ArrayList<>();
+    private static final List<Member> userRepository = new ArrayList<>();
     private static long sequence = 0L;
 
-    public User save(User user) {
+    public Member save(Member user) {
         user.setUserSequence(++sequence);
         userRepository.add(user);
         return user;
     }
 
-    public List<User> showAllUsers() {
-        List<User> allUsers = new ArrayList<>();
+    public List<Member> showAllUsers() {
+        List<Member> allMembers = new ArrayList<>();
         for (int i = 0; i < userRepository.size(); i++) {
-            allUsers.add(userRepository.get(i));
+            allMembers.add(userRepository.get(i));
         }
-        return allUsers;
+        return allMembers;
     }
 
-    public User findByUserId(String userId) {
+    public Member findByUserId(String userId) {
         return userRepository.stream().filter(user -> user.getUserId().equals(userId)).findFirst().orElseThrow();
     }
 
-    public void updateUser(String userId, User updateParam) {
-        User user = findByUserId(userId);
+    public void updateUser(String userId, Member updateParam) {
+        Member user = findByUserId(userId);
         String originalPassword = user.getPassword();
         if (!originalPassword.equals(updateParam.getPassword())) {
             return;
