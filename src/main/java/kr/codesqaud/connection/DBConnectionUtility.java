a7f03dde8e96d0365585d7a4a64a5673d38de2b4
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-21 21:28:03 +0900

    Feat: DBConnectionUtility.java 기능 추가
    상수 처리한 URL, USERNAME, PASSWORD를 이용하여 Connection을 생성하는 기능 추가

--- src/main/java/kr/codesqaud/connection/DBConnectionUtility.java
+++ src/main/java/kr/codesqaud/connection/DBConnectionUtility.java
@@ -1,5 +1,19 @@
 package kr.codesqaud.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static kr.codesqaud.connection.ConnectionConst.*;
+
 public class DBConnectionUtility {
 
+	public static Connection getConnection() {
+		try {
+			Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			return connection;
+		} catch (SQLException e) {
+			throw new IllegalStateException(e);
+		}
+	}
 }
