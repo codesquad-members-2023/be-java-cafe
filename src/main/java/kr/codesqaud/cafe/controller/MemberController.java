c7a97b967af8198153f95566ff1751b53acd177a
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-22 00:27:32 +0900

    Refactor: MemberController.java 코드 수정
    User가 SQL의 예약어인 관계로 기존의 User를 Member로 수정함에 따라 발생한 코드 수정

--- src/main/java/kr/codesqaud/cafe/controller/MemberController.java
+++ src/main/java/kr/codesqaud/cafe/controller/MemberController.java
@@ -1,7 +1,7 @@
 package kr.codesqaud.cafe.controller;
 
-import kr.codesqaud.cafe.domain.user.User;
-import kr.codesqaud.cafe.domain.user.UserRepository;
+import kr.codesqaud.cafe.domain.user.Member;
+import kr.codesqaud.cafe.domain.user.MemberRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +14,14 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/users")
-public class UserController {
+public class MemberController {
 
-    private final UserRepository userRepository;
-    private final Logger log = LoggerFactory.getLogger(UserController.class);
+    private final MemberRepository memberRepository;
+    private final Logger log = LoggerFactory.getLogger(MemberController.class);
 
     @Autowired
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public MemberController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @GetMapping("/add")
@@ -30,8 +30,8 @@ public class UserController {
     }
 
     @PostMapping("/add")
-    public String saveUser(@ModelAttribute("user") User user) {
-        userRepository.save(user);
+    public String saveUser(@ModelAttribute("user") Member user) {
+        memberRepository.save(user);
         log.trace("사용자 ID: {}", user.getUserId());
         log.trace("사용자 이름: {}", user.getName());
         log.trace("사용자 email: {}", user.getEmail());
@@ -40,28 +40,28 @@ public class UserController {
 
     @GetMapping("/list")
     public String users(Model model) {
-        List<User> users = userRepository.showAllUsers();
-        model.addAttribute("users", users);
-        log.trace("사용자 수: {}", users.size());
+        List<Member> members = memberRepository.showAllUsers();
+        model.addAttribute("users", members);
+        log.trace("사용자 수: {}", members.size());
         return "user/list";
     }
 
     @GetMapping("/profile/{userId}")
     public String userProfile(@PathVariable String userId, Model model) {
-        User findUser = userRepository.findByUserId(userId);
+        Member findUser = memberRepository.findByUserId(userId);
         model.addAttribute("user", findUser);
         return "user/profile";
     }
 
     @GetMapping("/update/{userId}")
     public String editUserForm(@PathVariable String userId, Model model) {
-        model.addAttribute("user", userRepository.findByUserId(userId));
+        model.addAttribute("user", memberRepository.findByUserId(userId));
         return "user/updateForm";
     }
 
     @PutMapping("/update/{userId}")
-    public String edit(@PathVariable String userId, @ModelAttribute User user) {
-        userRepository.updateUser(userId, user);
+    public String edit(@PathVariable String userId, @ModelAttribute Member user) {
+        memberRepository.updateUser(userId, user);
         return "redirect:/users/list";
     }
 
@@ -70,9 +70,9 @@ public class UserController {
      */
     @PostConstruct
     public void init() {
-        userRepository.save(new User("GOMUNGNAM", "247597", "고건호", "rhrjsgh97@gmail.com"));
-        userRepository.save(new User("Roy", "123456", "이승로", "roy@gmail.com"));
-        userRepository.save(new User("Birdie", "123456", "임동현", "birdie@gmail.com"));
-        userRepository.save(new User("Hana", "1234", "왕하나", "hana@gmail.com"));
+        memberRepository.save(new Member("GOMUNGNAM", "247597", "고건호", "rhrjsgh97@gmail.com"));
+        memberRepository.save(new Member("Roy", "123456", "이승로", "roy@gmail.com"));
+        memberRepository.save(new Member("Birdie", "123456", "임동현", "birdie@gmail.com"));
+        memberRepository.save(new Member("Hana", "1234", "왕하나", "hana@gmail.com"));
     }
 }
