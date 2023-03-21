d7fcea99c38c6da9ed2f028e4b0e96640adcd9ec
JeonHyoChang <jhc1426@naver.com>
2023-03-21 14:25:47 +0900

    ♻️refactor: list리버스 기능 -> ORDER BY 로 변경 & 컬럼 변경

--- src/main/java/kr/codesqaud/cafe/repository/JdbcTemplateArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/JdbcTemplateArticleRepository.java
@@ -8,10 +8,8 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Repository
 public class JdbcTemplateArticleRepository implements ArticleRepository{
@@ -23,40 +21,43 @@ public class JdbcTemplateArticleRepository implements ArticleRepository{
     @Override
     public boolean saveArticle(Article article) {
         // 인덱스 중복 여부 확인
-        if (findByIndex(article.getIndex()).isEmpty()) {
+        if (findById(article.getId()).isEmpty()) {
             SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
-            jdbcInsert.withTableName("cafe_article").usingGeneratedKeyColumns("index");
+            jdbcInsert.withTableName("cafe_article").usingGeneratedKeyColumns("id");
 
             Map<String, Object> parameters = new HashMap<>();
             parameters.put("writer", article.getWriter());
             parameters.put("title", article.getTitle());
             parameters.put("contents", article.getContents());
+            parameters.put("time", LocalDateTime.now());
 
             Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
-            article.setIndex(key.longValue());
+            article.setId(key.longValue());
             return true;
         }
         return false;
     }
 
     @Override
-    public Optional<Article> findByIndex(long index) {
-        List<Article> result = jdbcTemplate.query("select * from cafe_article where index = ?", articleRowMapper(), index);
+    public Optional<Article> findById(long id) {
+        List<Article> result = jdbcTemplate.query("select * from cafe_article where id = ?", articleRowMapper(), id);
         return result.stream().findAny();
     }
 
     @Override
     public List<Article> findAllArticle() {
-        return jdbcTemplate.query("select * from cafe_article", articleRowMapper());
+        List<Article> result = jdbcTemplate.query("select * from cafe_article ORDER BY id desc ", articleRowMapper());
+        return result;
     }
 
     private RowMapper<Article> articleRowMapper() {
         return (rs, rowNum) -> {
             Article article = new Article();
-            article.setIndex(rs.getLong("index"));
+            article.setId(rs.getLong("id"));
             article.setWriter(rs.getString("writer"));
             article.setTitle(rs.getString("title"));
             article.setContents(rs.getString("contents"));
+            article.setTime(rs.getTimestamp("time"));
             return article;
         };
     }
