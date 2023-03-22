1d43f94e17c0a198401e374603cb9df35eff9a3b
saehim <saeilee.vxv@gmail.com>
2023-03-22 20:11:19 +0900

    Feat (exceptionHandler) : 에러 메시지가 없는 경우, 알수없음으로 출력

--- src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice   // 모든 컨트롤러에 적용
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
-    private String noSuchException(Exception e, Model model) {
-        model.addAttribute("message", e.getMessage());
+    private String exception(Exception e, Model model) {
         e.printStackTrace();
+        model.addAttribute("message", "알 수 없는 에러가 발생했습니다.");
+
+        if (e.getMessage()!=null) {
+            model.addAttribute("message", e.getMessage());
+        }
         return "layout/error";
     }
 }
