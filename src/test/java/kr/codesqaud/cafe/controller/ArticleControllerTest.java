fd08df806e1e737ac9a9628b8d10e4ca4da7e060
lvalentine6 <ysr1227@naver.com>
2023-03-22 18:55:17 +0900

    Test : Test 코드 뼈대 생성

---
+++ src/test/java/kr/codesqaud/cafe/controller/ArticleControllerTest.java
@@ -0,0 +1,20 @@
+package kr.codesqaud.cafe.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArticleControllerTest {
+
+    @Test
+    void creatArticle() {
+    }
+
+    @Test
+    void findArticleList() {
+    }
+
+    @Test
+    void findArticleById() {
+    }
+}
