3c503feccad2750677d134b5a8ea2130cddde9b5
ngw7617@naver.com <ngw7617@naver.com>
2023-03-22 14:54:34 +0900

    feat(step-4):예외처리 View 구현 및 예외 Handler 이름 변경, View 수정
    - 댓글 개수, 내용 수정
    - 예외 발생 시 error view를 보여주도록 구현

--- src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package kr.codesqaud.cafe.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
-public class Handler {
+@ControllerAdvice
+public class GlobalExceptionHandler {
     @ExceptionHandler(value = IllegalArgumentException.class)
     public String noneMatchingException(IllegalArgumentException e, Model model) {
-        model.addAttribute("error", e.getMessage());
+        model.addAttribute("error",e.getMessage());
 
-        return "exception/error";
+        return "exception/profile";
     }
 
 
