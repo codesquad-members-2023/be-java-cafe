1bdb2d3629a5d80eaa4b1547017ba19f57e6bd1b
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-22 16:42:49 +0900

    Feat: Article.java 기능 추가
    게시글이 써진 시간을 저장하는 writtenTime을 객체 생성 시에 초기화하는 기능 추가

--- src/main/java/kr/codesqaud/cafe/domain/article/Article.java
+++ src/main/java/kr/codesqaud/cafe/domain/article/Article.java
@@ -1,47 +1,63 @@
 package kr.codesqaud.cafe.domain.article;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
 public class Article {
 
-    private Long articleSequence;
-    private String writer;
-    private String title;
-    private String contents;
+	private Long articleSequence;
+	private String writer;
+	private String title;
+	private String contents;
+	private String writtenTime;
 
-    public Article(String writer, String title, String contents) {
-        this.writer = writer;
-        this.title = title;
-        this.contents = contents;
-    }
+	public Article(String writer, String title, String contents) {
+		this.writer = writer;
+		this.title = title;
+		this.contents = contents;
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		writtenTime = formattingDate(timestamp);
+	}
 
-    public Long getArticleSequence() {
-        return articleSequence;
-    }
+	public Long getArticleSequence() {
+		return articleSequence;
+	}
 
-    public void setArticleSequence(Long articleSequence) {
-        this.articleSequence = articleSequence;
-    }
+	public void setArticleSequence(Long articleSequence) {
+		this.articleSequence = articleSequence;
+	}
 
-    public String getWriter() {
-        return writer;
-    }
+	public String getWriter() {
+		return writer;
+	}
 
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getContents() {
-        return contents;
-    }
+	public String getContents() {
+		return contents;
+	}
 
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getWrittenTime() {
+		return writtenTime;
+	}
+
+	private String formattingDate(Timestamp timestamp) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String formattedDate = simpleDateFormat.format(timestamp).toString();
+		return formattedDate;
+	}
 }
