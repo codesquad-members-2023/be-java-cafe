e2fb84643affafd2cbea710b83c5423256176bf1
JeonHyoChang <jhc1426@naver.com>
2023-03-22 17:03:47 +0900

    ♻️refactor: 필요없는 model attribute 제거

--- src/main/java/kr/codesqaud/cafe/controller/LoginController.java
+++ src/main/java/kr/codesqaud/cafe/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,30 +25,26 @@ public class LoginController {
 
     // 로그인 기능
     @PostMapping("/login")
-    public String login(String userId, String password, HttpSession session, Model model) {
+    public String login(String userId, String password, HttpServletRequest request) {
         User loginUser = repository.findByUserId(userId)
                 .filter(u -> u.getPassword().equals(password))
                 .orElse(null);
         if (loginUser == null) {
-
             log.debug("로그인 실패! ㅠㅠ");
-            model.addAttribute("loginFailCheck", true);
-            return "user/login";
+            return "user/login_failed";
         }
+        HttpSession session = request.getSession();
 
         // 로그인 성공 처리
         log.debug("로그인 성공!!!");
-
-        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
-        //HttpSession session1 = requset.getSession(); <- 디폴트가 true 라서 안써도됨
         // 세션에 로그인 회원 정보 보관
-        session.setAttribute("loginUser",loginUser);
+        session.setAttribute("loginUser", loginUser);
 
         return "redirect:/";
     }
 
     // 로그아웃
-    @PostMapping("/logout")
+    @GetMapping("/logout")
     public String logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session != null) {
