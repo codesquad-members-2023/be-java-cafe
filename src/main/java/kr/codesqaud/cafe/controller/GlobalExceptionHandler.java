fef21872645cdb72f199b4091017d5bd1d908066
ngw7617@naver.com <ngw7617@naver.com>
2023-03-22 20:36:42 +0900

    fix(step-4):Git 충돌로 인한 Intellij 코드 복구 기능

--- src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
@@ -3,16 +3,14 @@ package kr.codesqaud.cafe.controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = IllegalArgumentException.class)
     public String noneMatchingException(IllegalArgumentException e, Model model) {
-        model.addAttribute("error",e.getMessage());
+        model.addAttribute("error", e.getMessage());
 
-        return "exception/profile";
+        return "exception/error";
     }
 
 
--- src/main/java/kr/codesqaud/cafe/model/Article.java
+++ src/main/java/kr/codesqaud/cafe/model/Article.java
@@ -1,17 +1,16 @@
 package kr.codesqaud.cafe.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.cglib.core.Local;
+
 public class Article {
     private long id;
     private String writer;
     private String title;
     private String contents;
-
-    public Article(long id, String writer, String title, String contents) {
-        this.id = id;
-        this.writer = writer;
-        this.title = title;
-        this.contents = contents;
-    }
+    private LocalDateTime creationTime;
 
     public Article(long id, String writer, String title, String contents, LocalDateTime creationTime) {
         this.id = id;
@@ -28,14 +27,8 @@ public class Article {
         creationTime = LocalDateTime.now();
     }
 
-    public String getFormattedCreationTime() {
-        return creationTime.format(
-                DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
-    }
-
-    public String getCreationTime() {
-        return creationTime.format(
-                DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+    public LocalDateTime getCreationTime() {
+        return creationTime;
     }
 
     public String getFormattedCreationTime() {
