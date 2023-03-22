f78743f106b07d1b2749269e4c8f526166c1dc2e
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-22 16:51:32 +0900

    Feat: ddl.sql 추가 작성
    article 테이블 구조 추가 작성

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,10 +1,22 @@
 drop table if exists member cascade;
 
-create table member (
-    member_number bigint not null auto_increment,
-    member_id varchar(64) not null,
+create table member
+(
+    member_number   bigint      not null auto_increment,
+    member_id       varchar(64) not null,
     member_password varchar(64) not null,
-    member_name varchar(64) not null,
-    member_email varchar(64) not null,
+    member_name     varchar(64) not null,
+    member_email    varchar(64) not null,
     primary key (member_id)
+);
+
+create table article
+(
+    article_number      bigint        not null auto_increment,
+    article_writer      varchar(64)   not null,
+    article_title       varchar(200)  not null,
+    article_contents    varchar(1000) not null,
+    article_writtentime varchar(64)   not null,
+    primary key (article_number);
+
 );
