906016c4064ac750b664567eb093879d0b89edb5
Hyun <ghkdgus29@naver.com>
2023-03-23 13:39:15 +0900

    feat: 로그인 여부를 체크하는 로그인 인터셉터 추가
    로그인하지 않은 사용자는 글작성 및 글 세부내용을 볼 수 없다
    해당 동작을 수행하려고 하면 로그인 페이지로 리디렉션 처리한다.

---
+++ src/main/java/kr/codesqaud/cafe/config/WebConfig.java
@@ -0,0 +1,18 @@
+package kr.codesqaud.cafe.config;
+
+import kr.codesqaud.cafe.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/questions/form", "/articles/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/fonts/**", "/images/**", "/js/**");
+    }
+}
