344f438c6d37e24fd9bef443b3c674afa7fd89e8
birdieHyun <flaehdan@gmail.com>
2023-03-23 18:26:49 +0900

    Feat : 자동으로 DB 초기화
    스프링부트 실행시마다 데이터 초기화 진행

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into member values ('first','password', 'first-name', 'first@email.com');
+insert into member values ('second','password', 'second-name', 'second@email.com');
+
+insert into article (writer, title, contents, timestamp) values ('first', 'first-title', 'first-contents', now());
+insert into article (writer, title, contents, timestamp) values ('second', 'second-title', 'second-contents', now());
