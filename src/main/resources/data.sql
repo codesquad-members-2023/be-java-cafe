610c71c4eb0834dc9c10d25ce6b7424409f1cd92
jaea-kim <jaeakim98@gmail.com>
2023-03-23 19:22:50 +0900

    필요없는 파일 삭제 및 html 중복 제거

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO MEMBERS (id, password, name, email) VALUES ('dewey', '1111', 'jaea', 'dewey@ac.kr');
+INSERT INTO MEMBERS (id, password, name, email) VALUES ('honux', '2222', 'hhhh', 'honux@ac.kr');
