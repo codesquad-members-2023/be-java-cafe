eacb66447a98b4157198435c2702ed148e23efe9
JeonHyoChang <jhc1426@naver.com>
2023-03-23 11:46:06 +0900

    ✨feat: 필터를 활용해 로그인 유저만 접근하도록 제어 하는 기능 추가

---
+++ src/main/java/kr/codesqaud/cafe/filter/LogFilter.java
@@ -0,0 +1,45 @@
+package kr.codesqaud.cafe.filter;
+
+import kr.codesqaud.cafe.controller.UserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+public class LogFilter implements Filter {
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+        //Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+        //Filter.super.destroy();
+    }
+}
---
+++ src/main/java/kr/codesqaud/cafe/repository/SessionConst.java
@@ -0,0 +1,5 @@
+package kr.codesqaud.cafe.repository;
+
+public class SessionConst {
+    public static final String LOGIN_USER = "loginUser";
+}
