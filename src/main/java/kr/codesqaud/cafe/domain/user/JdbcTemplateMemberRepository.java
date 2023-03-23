f09c0a06f5b30f035bd3dc21226076a1dbafe207
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-23 19:35:07 +0900

    Feat: JdbcTemplateMemberRepository.java 기능 추가
    RowMapper<Member>를 반환하는 memberRowMapper() 메서드 추가

--- src/main/java/kr/codesqaud/cafe/domain/user/JdbcTemplateMemberRepository.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/JdbcTemplateMemberRepository.java
@@ -1,7 +1,36 @@
 package kr.codesqaud.cafe.domain.user;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class JdbcTemplateMemberRepository {
 
+	private final JdbcTemplate jdbcTemplate;
 
+	// 생성자가 단 한개만 있으면 @Autowired 생략 가능
+	public JdbcTemplateMemberRepository(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
+	public Member findById(String memberId) {
+
+	}
+
+	private RowMapper<Member> memberRowMapper() {
+		return new RowMapper<Member>() {
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Member member = new Member();
+				member.setUserId(rs.getString("member_id"));
+				member.setPassword(rs.getString("member_password"));
+				member.setName(rs.getString("member_name"));
+				member.setEmail(rs.getString("member_email"));
+				return member;
+			}
+		};
+	}
 }
