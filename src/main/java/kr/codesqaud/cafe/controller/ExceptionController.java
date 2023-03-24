4fd2ee40cd22bb89aca83d3d559dfcf5f2804242
birdieHyun <flaehdan@gmail.com>
2023-03-24 17:58:01 +0900

    Refactor : 에러 페이지 상태코드 변경
    200 OK에서 400으로 변경

--- src/main/java/kr/codesqaud/cafe/controller/ExceptionController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ExceptionController.java
@@ -1,10 +1,13 @@
 package kr.codesqaud.cafe.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ExceptionController {
 
     @ExceptionHandler(IllegalArgumentException.class)
