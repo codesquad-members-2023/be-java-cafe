03954d327327f4dcc33866c695a748240880e1dd
saehim <saeilee.vxv@gmail.com>
2023-03-24 09:47:37 +0900

    Update (exception) : `ManagerMemberException` 예외시 `ExceptionStatus`에 따라 다르게 동작할 수 있도록 수정

--- src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
@@ -1,38 +1,56 @@
 package kr.codesqaud.cafe.controller;
 
-import kr.codesqaud.cafe.exception.LoginFailException;
+import kr.codesqaud.cafe.domain.Member;
+import kr.codesqaud.cafe.exception.ExceptionStatus;
+import kr.codesqaud.cafe.exception.ManageMemberException;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice   // 모든 컨트롤러에 적용
 public class GlobalExceptionHandler {
-    @ExceptionHandler(LoginFailException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNoSuchElementFoundException(LoginFailException e) {
-        return "user/login_failed";
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = ManageMemberException.class)
+    public String handleFailToManageMemberException(ManageMemberException e, Model model, RedirectAttributes redirectAttributes, HttpSession httpSession) {
+        model.addAttribute("message", e.getMessage());
+
+        switch (e.getStatus()) {
+            case NO_SESSION_USER:
+            case LOGIN_FAIL:
+            case DUPLICATE_MEMBER_INFO:
+                return "user/login_failed";
+            case WRONG_PASSWORD:
+                redirectAttributes.addAttribute("message", e.getMessage());
+                String id = ((Member) httpSession.getAttribute("sessionedUser")).getUserId();
+                redirectAttributes.addFlashAttribute(id);
+                return "redirect:/user/{id}/update";
+            default: return "error";
+        }
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNoSuchElementFoundException(NoSuchElementException e, Model model) {
         model.addAttribute("message", e.getMessage());
-        return "layout/error";
+        return "error";
     }
 
     @ExceptionHandler(value = Exception.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     private String exception(Exception e, Model model) {
         e.printStackTrace();
-        model.addAttribute("message", "알 수 없는 에러가 발생했습니다.");
+        model.addAttribute("message", "에러가 발생했습니다.");
 
         if (e.getMessage()!=null) {
             model.addAttribute("message", e.getMessage());
         }
-        return "layout/error";
+        return "error";
     }
 }
