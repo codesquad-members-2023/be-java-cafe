d76e8a388f2494ae91158d4c326e23d65a28b0b4
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-24 20:02:59 +0900

    Feat: MemberRepositoryTest.java 기능 추가
    Logger를 통해 각 테스트 별로 로그를 찍는 기능 추가

--- src/test/java/kr/codesqaud/cafe/domain/user/MemberRepositoryTest.java
+++ src/test/java/kr/codesqaud/cafe/domain/user/MemberRepositoryTest.java
@@ -2,8 +2,8 @@ package kr.codesqaud.cafe.domain.user;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +28,11 @@ class MemberRepositoryTest {
 
 		Member findMember = repository.findById(test.getUserId()).orElseThrow();
 
+		logger.info("test.getName():{}, findMember.getName():{}", test.getName(), findMember.getName());
+		logger.info("test.getPassword():{}, findMember.getPassword():{}", test.getPassword(), findMember.getPassword());
+		logger.info("test.getEmail():{}, findMember.getEmail():{}", test.getEmail(), findMember.getEmail());
+		logger.info("test.getUserId():{}, findMember.getUserId():{}", test.getUserId(), findMember.getUserId());
+
 		assertThat(test.getName()).isEqualTo(findMember.getName());
 		assertThat(test.getPassword()).isEqualTo(findMember.getPassword());
 		assertThat(test.getEmail()).isEqualTo(findMember.getEmail());
@@ -41,6 +46,9 @@ class MemberRepositoryTest {
 		String userId2 = "noeseyhoes";
 		Member findMember1 = repository.findById(userId1).orElseThrow();
 		Member findMember2 = repository.findById(userId2).orElseThrow();
+
+		logger.info("findMember1.getUserId():{}, userId1:{}", findMember1.getUserId(), userId1);
+		logger.info("findMember2.getUserId():{}, userId2:{}", findMember2.getUserId(), userId2);
 
 		assertThat(findMember1.getUserId()).isEqualTo(userId1);
 		assertThat(findMember2.getUserId()).isEqualTo(userId2);
@@ -60,6 +68,10 @@ class MemberRepositoryTest {
 	@DisplayName("비밀번호가 일치 시에 회원 정보를 수정하는 테스트")
 	void updateSuccessed() {
 		Member updateParam = new Member("rhrjsgh97", "1234", "고봉렬", "gobongyeol@gmail.com");
+
+		logger.info("original password:{}, updateParam password:{}", repository.findById(updateParam.getUserId()).orElseThrow().getPassword(), updateParam.getPassword());
+		logger.info("original password == updateParam password:{}", repository.findById(updateParam.getUserId()).orElseThrow().getPassword().equals(updateParam.getPassword()));
+
 		repository.update(updateParam);
 
 		Member updatedMember = repository.findById("rhrjsgh97").orElseThrow();
@@ -71,6 +83,10 @@ class MemberRepositoryTest {
 	@DisplayName("비밀번호 불일치 시에 회원 정보 수정이 이뤄지지 않는 테스트")
 	void updateFailed() {
 		Member updateParam = new Member("rhrjsgh97", "5678", "고봉렬", "gobongyeol@gmail.com");
+
+		logger.info("original password:{}, updateParam password:{}", repository.findById(updateParam.getUserId()).orElseThrow().getPassword(), updateParam.getPassword());
+		logger.info("original password == updateParam password:{}", repository.findById(updateParam.getUserId()).orElseThrow().getPassword().equals(updateParam.getPassword()));
+
 		repository.update(updateParam);
 
 		Member updatedMember = repository.findById("rhrjsgh97").orElseThrow();
