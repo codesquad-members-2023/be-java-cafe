9fd63fd3dce5d8d12ba139dd867671a7bf6725bb
saehim <saeilee.vxv@gmail.com>
2023-03-25 14:54:30 +0900

    Refactor : 사용하지 않은 라인 삭제

--- src/test/java/kr/codesqaud/cafe/repository/H2ArticleRepositoryTest.java
+++ src/test/java/kr/codesqaud/cafe/repository/H2ArticleRepositoryTest.java
@@ -5,11 +5,8 @@ import kr.codesqaud.cafe.domain.Member;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
