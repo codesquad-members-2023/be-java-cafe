c7cbdab3c5c4ea50dd1453f4f65f55a2aae175ee
Life is a ninanino <tvxqinak@naver.com>
2023-03-26 01:35:15 +0900

    feat : 검증 로직 추가
    세션아이디와 글 작성자를 비교하는 검증 로직

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -1,15 +1,20 @@
 package kr.codesqaud.cafe.controller;
 
 import kr.codesqaud.cafe.domain.Article;
+import kr.codesqaud.cafe.domain.Member;
 import kr.codesqaud.cafe.repository.article.JdbcArticleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Controller
@@ -26,7 +31,7 @@ public class ArticleController {
 
     @PostMapping("/questions")
     public String save(@ModelAttribute Article article) {
-        logger.debug("addContents {}",LocalDateTime.now());
+        logger.debug("addContents {}", LocalDateTime.now());
         jdbcArticleRepository.save(article);
         return "redirect:/";
     }
@@ -44,7 +49,7 @@ public class ArticleController {
         Optional<Article> article = jdbcArticleRepository.findById(id);
         System.out.println(article.isPresent());
         if (article.isPresent()) {
-            model.addAttribute("articles", article.orElseThrow(IllegalArgumentException::new));
+            model.addAttribute("articles", article.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 게시글이 없습니다.")));
             return "qna/show";
         } else {
             return "redirect:/";
@@ -73,5 +78,16 @@ public class ArticleController {
         jdbcArticleRepository.delete(id);
         return "redirect:/";
     }
+
+    public void vaildateAuthorization(HttpSession session, Article article) {
+        Member value = (Member) session.getAttribute("sessionedUser");
+        String loginId = value.getUserId();
+        String writerId = article.getAuthorId();
+        if (loginId != writerId) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
 }
 
