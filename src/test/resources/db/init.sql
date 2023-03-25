18ac5cd9a24b357a842d5cc4b7331f4f03a3a4e0
saehim <saeilee.vxv@gmail.com>
2023-03-25 12:39:36 +0900

    Setup (db) : 본 환경과 똑같은 테스트 db 설정
    - 자동으로 시작될 때 실행되므로 별도 properties 설정이 필요없었다.
    - table과 기본 데이터 쿼리를 분리하여 관리하였다.

---
+++ src/test/resources/db/init.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS member;
+
+create table member (
+                        id  long auto_increment primary key,
+                        userId   varchar(255) not null unique,
+                        nickName varchar(255) not null,
+                        email   varchar(500) not null unique,
+                        password    varchar(255) not null,
+                        created_at  timestamp default current_timestamp(),
+                        updated_at  timestamp default current_timestamp()
+);
+
+create table article (
+                         id  long auto_increment primary key,
+                         title   varchar(255) not null,
+                         body    text not null,
+                         user_id long not null,
+                         created_at  timestamp default current_timestamp(),
+                         updated_at  timestamp default current_timestamp(),
+                         constraint ARTICLE_MEMBER_ID_FK
+                             foreign key (user_id) references member (id)
+);
---
+++ src/test/resources/db/initData.sql
@@ -0,0 +1,8 @@
+INSERT INTO MEMBER (userId, password, nickName, email)
+VALUES
+    ('javajigi', '1234', '자바지기', 'javajigi@slipp.net'),
+    ('sanjigi', '1234', '산지기', 'sanjigi@slipp.net');
+
+INSERT INTO article (title, body, user_id)
+VALUES ('test1','test content1', 1),
+       ('test2','test content2', 2);
