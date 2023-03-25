0d52b4995d11bcd7ab5fcfcf6af7ba03558c888c
saehim <saeilee.vxv@gmail.com>
2023-03-25 13:28:30 +0900

    Feat (repository) : `jdbcTemplate` -> `namedParameterJdbcTemplate` 변경

--- src/main/java/kr/codesqaud/cafe/repository/H2MemberRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/H2MemberRepository.java
@@ -3,6 +3,8 @@ package kr.codesqaud.cafe.repository;
 import kr.codesqaud.cafe.domain.Member;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
@@ -14,33 +16,38 @@ import java.util.*;
 public class H2MemberRepository implements MemberRepository {
 
     private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public H2MemberRepository(DataSource dataSource) {
-        jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
     @Override
     public void save(Member member) {
         if (validMemberId(member.getNickname())) {
-            String sql = "INSERT INTO MEMBER (USERID, NICKNAME, EMAIL, PASSWORD) VALUES (?, ?, ?, ?)";
-            jdbcTemplate.update(sql,
-                    member.getUserId(),
-                    member.getNickname(),
-                    member.getEmail(),
-                    member.getPassword());
+            String sql = "INSERT INTO MEMBER (USERID, NICKNAME, EMAIL, PASSWORD) VALUES (:userId, :nickName, :email, :password)";
+            MapSqlParameterSource params = new MapSqlParameterSource();
+            params.addValue("userId", member.getUserId())
+                .addValue("nickName", member.getNickname())
+                .addValue("email", member.getEmail())
+                .addValue("password", member.getPassword());
+            namedParameterJdbcTemplate.update(sql, params);
         }
     }
 
     @Override
-    public Optional<Member> findById(Long userId) {
-        String sql = "SELECT ID, USERID, NICKNAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT FROM MEMBER WHERE ID = ?";
-        return jdbcTemplate.query(sql, new MemberRowMapper(), userId).stream().findAny();
+    public Member findById(Long userId) {
+        String sql = "SELECT ID, USERID, NICKNAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT FROM MEMBER WHERE ID = :id";
+        MapSqlParameterSource param = new MapSqlParameterSource("id", userId);
+        return namedParameterJdbcTemplate.queryForObject(sql, param, new MemberRowMapper());
     }
 
     @Override
-    public Optional<Member> findByMemberId(String userId) {
-        String sql = "SELECT ID, USERID, NICKNAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT FROM MEMBER WHERE USERID = ?";
-        return jdbcTemplate.query(sql, new MemberRowMapper(), userId).stream().findAny();
+    public Member findByMemberId(String userId) {
+        String sql = "SELECT ID, USERID, NICKNAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT FROM MEMBER WHERE USERID = :userId";
+        MapSqlParameterSource param = new MapSqlParameterSource("userId", userId);
+        return namedParameterJdbcTemplate.queryForObject(sql, param, new MemberRowMapper());
     }
 
     @Override
@@ -58,8 +65,14 @@ public class H2MemberRepository implements MemberRepository {
     @Override
     public void update(Member exMember, Member newUser) throws NoSuchElementException {
         Member updatedMember = exMember.update(newUser);
-        String sql = "UPDATE MEMBER SET NICKNAME = ?, EMAIL = ?, PASSWORD = ? WHERE USERID = ?";
-        jdbcTemplate.update(sql, updatedMember.getNickname(), updatedMember.getEmail(), updatedMember.getPassword(), updatedMember.getUserId());
+        String sql = "UPDATE MEMBER SET NICKNAME = :nickname, EMAIL = :email, PASSWORD = :password, UPDATED_AT = :updated WHERE USERID = :userId";
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("nickname", updatedMember.getNickname())
+            .addValue("email", updatedMember.getNickname())
+            .addValue("password", updatedMember.getUpdatedDate())
+            .addValue("updated", updatedMember.getUpdatedDate())
+            .addValue("userId", updatedMember.getId());
+        namedParameterJdbcTemplate.update(sql, params);
     }
 
     private static final class MemberRowMapper implements RowMapper<Member> {
