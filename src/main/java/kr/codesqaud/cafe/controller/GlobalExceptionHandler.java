55c988edda28d5324c01e66354580febbc35f8ee
saehim <saeilee.vxv@gmail.com>
2023-03-25 13:09:00 +0900

    Refactor : 자주 쓰는 변수 상수화
    - 에러메시지 전달 이름, 기본 에러페이지 상수화

--- src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/controller/GlobalExceptionHandler.java
@@ -1,9 +1,7 @@
 package kr.codesqaud.cafe.controller;
 
-import kr.codesqaud.cafe.domain.Member;
-import kr.codesqaud.cafe.exception.ExceptionStatus;
+import kr.codesqaud.cafe.dto.SessionUser;
 import kr.codesqaud.cafe.exception.ManageMemberException;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,43 +12,48 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpSession;
 import java.util.NoSuchElementException;
 
-@ControllerAdvice   // 모든 컨트롤러에 적용
+import static kr.codesqaud.cafe.dto.SessionUser.SESSION_USER;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final String MESSAGE = "message";
+    private static final String DEFAULT_ERROR_PAGE = "error";
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = ManageMemberException.class)
     public String handleFailToManageMemberException(ManageMemberException e, Model model, RedirectAttributes redirectAttributes, HttpSession httpSession) {
-        model.addAttribute("message", e.getMessage());
+        model.addAttribute(MESSAGE, e.getMessage());
 
         switch (e.getStatus()) {
             case NO_SESSION_USER:
-            case LOGIN_FAIL:
+            case LOGIN_FAILED:
             case DUPLICATE_MEMBER_INFO:
                 return "user/login_failed";
-            case WRONG_PASSWORD:
-                redirectAttributes.addAttribute("message", e.getMessage());
-                String id = ((Member) httpSession.getAttribute("sessionedUser")).getUserId();
+            case UPDATE_FAILED_WRONG_PASSWORD:
+                redirectAttributes.addAttribute(MESSAGE, e.getMessage());
+                long id = ((SessionUser) httpSession.getAttribute(SESSION_USER)).getId();
                 redirectAttributes.addFlashAttribute(id);
                 return "redirect:/user/{id}/update";
-            default: return "error";
+            default: return DEFAULT_ERROR_PAGE;
         }
     }
 
-    @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
     public String handleNoSuchElementFoundException(NoSuchElementException e, Model model) {
-        model.addAttribute("message", e.getMessage());
-        return "error";
+        model.addAttribute(MESSAGE, e.getMessage());
+        return DEFAULT_ERROR_PAGE;
     }
 
-    @ExceptionHandler(value = Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = Exception.class)
     private String exception(Exception e, Model model) {
         e.printStackTrace();
-        model.addAttribute("message", "에러가 발생했습니다.");
+        model.addAttribute(MESSAGE, "에러가 발생했습니다.");
 
         if (e.getMessage()!=null) {
-            model.addAttribute("message", e.getMessage());
+            model.addAttribute(MESSAGE, e.getMessage());
         }
-        return "error";
+        return DEFAULT_ERROR_PAGE;
     }
 }
