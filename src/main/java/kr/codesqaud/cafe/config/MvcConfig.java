8f3d7e38620b2f76940bf6d5b3ff276ee4e8cc10
Life is a ninanino <tvxqinak@naver.com>
2023-03-26 01:28:45 +0900

    feat :  loginInterceptor 추가
    로그인 시 세션을 확인해서 세션이 없는 경우 로그인 화면으로 전환하고 새션이 있는 경우 해당 컨트롤러 호출

--- src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
+++ src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
@@ -1,7 +1,9 @@
 package kr.codesqaud.cafe.config;
 
+import kr.codesqaud.cafe.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,8 +12,18 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
         registry.addViewController("/form").setViewName("user/form");
         registry.addViewController("/qna/questions").setViewName("qna/form");
+        //registry.addViewController("/articles/{id}/modified").setViewName("qna/modifiedForm");
+
+    }
+
+    @Override // 명시적으로 오버라이드
+    public void addInterceptors(InterceptorRegistry registry) { // 인터셉터 등록
+        WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login", "/css/**", "/images/**", "/error","/fonts/**");
     }
 }
---
+++ src/main/java/kr/codesqaud/cafe/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,21 @@
+package kr.codesqaud.cafe.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        HttpSession session = request.getSession(false);
+        if(session == null || session.getAttribute("sessionedUser") == null){
+            response.sendRedirect("/login"); // user/login이 아니여따..! url의 경로를 지정하는건데 html을 적어서 에러가 나고있었다
+            return false;
+        }
+        return true;
+    }
+
+}
