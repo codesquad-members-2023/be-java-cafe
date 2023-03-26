f44a102f0bafe04cfccc3be6c8ebf2a09269bbf6
CiRe <dltpwns6@naver.com>
2023-03-26 22:34:00 +0900

    Config : data 및 schema 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into articles(writer, title, contents ) values ('Article A','title A','contents A')
-insert into articles(writer, title, contents ) values ('Article B','title B','contents B')
-insert into articles(writer, title, contents ) values ('Article C','title C','contents C')
+insert into users(userId, password, name, email) values ('UserA', 'a', 'a', 'a@a')
+insert into users(userId, password, name, email) values ('UserB', 'b', 'b', 'b@b')
+insert into users(userId, password, name, email) values ('UserC', 'c', 'c', 'c@c')
 
-insert into users(userId, password, name, email) values ('a', 'a', 'a', 'a@a')
-insert into users(userId, password, name, email) values ('b', 'b', 'b', 'b@b')
-insert into users(userId, password, name, email) values ('c', 'c', 'c', 'c@c')
+insert into articles(writer, title, contents ) values ('UserA','title A','contents A')
+insert into articles(writer, title, contents ) values ('UserB','title B','contents B')
+insert into articles(writer, title, contents ) values ('UserC','title C','contents C')
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,6 @@
+drop table if exists ARTICLES;
 drop table if exists USERS;
+
 create table USERS  (
                         userId varchar(20) not null,
                         password varchar(20) not null,
@@ -6,13 +8,12 @@ create table USERS  (
                         email varchar(20) not null,
                         primary key(userId)
 );
-
-drop table if exists ARTICLES;
 create table ARTICLES  (
                            articleId int auto_increment,
                            writer varchar(20) not null,
                            title TINYTEXT not null,
                            contents TEXT not null,
                            timeStamp TIMESTAMP default current_timestamp,
-                           primary key(articleId)
+                           primary key(articleId),
+                           foreign key (writer) references USERS (userId) ON DELETE CASCADE
 );
