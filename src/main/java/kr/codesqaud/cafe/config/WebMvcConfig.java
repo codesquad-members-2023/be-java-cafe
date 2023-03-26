718ac7e14132881569a0cf2ffbbf941c58ac9b0b
birdieHyun <flaehdan@gmail.com>
2023-03-26 17:39:02 +0900

    Feat : 글 삭제하고 뒤로가기 시 캐시 삭제로 접근 방지하는 기능 추가

--- src/main/java/kr/codesqaud/cafe/config/WebMvcConfig.java
+++ src/main/java/kr/codesqaud/cafe/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package kr.codesqaud.cafe.config;
 
-import kr.codesqaud.cafe.interceptor.LoginIntercepter;
+import kr.codesqaud.cafe.interceptor.CacheInvalidInterceptor;
+import kr.codesqaud.cafe.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,8 +11,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginIntercepter())
+        registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/qna/**")
                 .addPathPatterns("/users/updateUser/**");
+
+        registry.addInterceptor(new CacheInvalidInterceptor())
+                .addPathPatterns("/**");
     }
 }
--- src/main/java/kr/codesqaud/cafe/interceptor/LoginInterceptor.java
+++ src/main/java/kr/codesqaud/cafe/interceptor/LoginInterceptor.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LoginIntercepter implements HandlerInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
