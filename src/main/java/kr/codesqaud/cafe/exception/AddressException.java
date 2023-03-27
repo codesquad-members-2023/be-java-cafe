7d04296e971ff341719031dfbe9cf3056dd15a36
birdieHyun <flaehdan@gmail.com>
2023-03-27 19:20:35 +0900

    Refactor : 예외처리를 RuntimeException 에서 Exception으로 변경
    외부에 영향을 받아서 발생할 수 있는 예외를 Exception 으로 변경

--- src/main/java/kr/codesqaud/cafe/exception/AddressException.java
+++ src/main/java/kr/codesqaud/cafe/exception/AddressException.java
@@ -1,6 +1,6 @@
 package kr.codesqaud.cafe.exception;
 
-public class AddressException extends RuntimeException {
+public class AddressException extends Exception {
     public AddressException(String message) {
         super(message);
     }
