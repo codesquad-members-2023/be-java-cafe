bfd83969df60753d7332f01ea6e933e40e0a6a25
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-27 17:50:42 +0900

    Feat: LoginService.java 생성
    로그인 로직이 담겨있는 LoginService.java 생성

---
+++ src/main/java/kr/codesqaud/cafe/domain/login/LoginService.java
@@ -0,0 +1,24 @@
+package kr.codesqaud.cafe.domain.login;
+
+import kr.codesqaud.cafe.domain.user.Member;
+import kr.codesqaud.cafe.dto.MemberLoginDto;
+import kr.codesqaud.cafe.repository.NamedJdbcTemplateMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+	private final NamedJdbcTemplateMemberRepository namedJdbcTemplateMemberRepository;
+
+	@Autowired
+	public LoginService(NamedJdbcTemplateMemberRepository namedJdbcTemplateMemberRepository) {
+		this.namedJdbcTemplateMemberRepository = namedJdbcTemplateMemberRepository;
+	}
+
+	public Member login(MemberLoginDto memberLoginDto) {
+		return namedJdbcTemplateMemberRepository.findById(memberLoginDto.getUserId())
+			.filter(member -> member.getPassword().equals(memberLoginDto.getPassword()))
+			.orElse(null);
+	}
+}
