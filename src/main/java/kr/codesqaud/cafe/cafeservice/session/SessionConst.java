f00ec4dbca1605032991ab378edd980cf3748855
leegyeongwhan <leekhy02@naver.com>
2023-03-27 17:16:27 +0900

    feat: 세샨 뱐수명 변경합니다.

--- src/main/java/kr/codesqaud/cafe/cafeservice/session/SessionConst.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/session/SessionConst.java
@@ -1,5 +1,5 @@
 package kr.codesqaud.cafe.cafeservice.session;
 
 public class SessionConst {
-    public final static String LOGIN_MEMBER = "loginMember";
+    public final static String LOGIN_MEMBER = "sessionedUser";
 }
