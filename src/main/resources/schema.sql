8b3f270e0d9e2b177748a7aa750b1d81911d818a
CiRe <dltpwns6@naver.com>
2023-03-27 17:22:53 +0900

    Test : 데이터베이스 스키마 및 데이터 입력 설정 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,34 @@
+drop table if exists REPLY;
 drop table if exists ARTICLES;
 drop table if exists USERS;
 
-create table USERS  (
-                        userId varchar(20) not null,
-                        password varchar(20) not null,
-                        name varchar(20) not null,
-                        email varchar(20) not null,
-                        primary key(userId)
+create table USERS
+(
+    userId   varchar(20) not null,
+    password varchar(20) not null,
+    name     varchar(20) not null,
+    email    varchar(20) not null,
+    primary key (userId)
 );
-create table ARTICLES  (
-                           articleId int auto_increment,
-                           writer varchar(20) not null,
-                           title TINYTEXT not null,
-                           contents TEXT not null,
-                           timeStamp TIMESTAMP default current_timestamp,
-                           primary key(articleId),
-                           foreign key (writer) references USERS (userId) ON DELETE CASCADE
+create table ARTICLES
+(
+    articleId int auto_increment,
+    writer    varchar(20) not null,
+    title     TINYTEXT    not null,
+    contents  TEXT        not null,
+    timeStamp TIMESTAMP default current_timestamp,
+    primary key (articleId),
+    foreign key (writer) references USERS (userId) ON DELETE CASCADE
 );
+create table REPLIES
+(
+    replyId int auto_increment,
+    articleId int not null,
+    writer varchar(20) not null,
+    content TINYTEXT not null,
+    createAt TIMESTAMP default current_timestamp,
+    primary key (replyId),
+    foreign key (articleId) references ARTICLES (articleId) ON DELETE CASCADE,
+    foreign key (writer) references USERS (userId) ON DELETE CASCADE
+)
+
