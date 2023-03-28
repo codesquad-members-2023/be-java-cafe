84ceac2e897107d45b7cc168c7af5fa6904963cf
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-28 14:02:15 +0900

    Feat: LoginController.java 기능 추가
    Constants.java에서 상수 선언한 변수를 사용하기 위한 import static 추가

--- src/main/java/kr/codesqaud/cafe/controller/LoginController.java
+++ src/main/java/kr/codesqaud/cafe/controller/LoginController.java
@@ -3,7 +3,6 @@ package kr.codesqaud.cafe.controller;
 import kr.codesqaud.cafe.domain.login.LoginService;
 import kr.codesqaud.cafe.domain.user.Member;
 import kr.codesqaud.cafe.dto.MemberLoginDto;
-import kr.codesqaud.cafe.repository.NamedJdbcTemplateMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,17 +11,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 
+import static kr.codesqaud.cafe.controller.Constants.SESSIONED_USER;
+
 @Controller
 public class LoginController {
 
 	private final LoginService loginService;
 
-	private final NamedJdbcTemplateMemberRepository namedJdbcTemplateMemberRepository;
-
 	@Autowired
-	public LoginController(LoginService loginService, NamedJdbcTemplateMemberRepository namedJdbcTemplateMemberRepository) {
+	public LoginController(LoginService loginService) {
 		this.loginService = loginService;
-		this.namedJdbcTemplateMemberRepository = namedJdbcTemplateMemberRepository;
 	}
 
 	@GetMapping("/login")
@@ -36,7 +34,7 @@ public class LoginController {
 		Member loginMember = loginService.login(memberLoginDto);
 
 		if (loginMember != null) {
-			session.setAttribute("sessionedUser", loginMember);
+			session.setAttribute(SESSIONED_USER, loginMember);
 			return "redirect:/";
 		}
 
