48bb8ed605f7d7924618fbc4d33aa8ebb62e3f45
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-28 15:35:07 +0900

    Feat: ArticleController.java 기능 추가
    로그인 하지 않은 상태에서 글을 쓰려고 할 경우 로그인 화면으로 연결해주는 기능 추가

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package kr.codesqaud.cafe.controller;
 
 import kr.codesqaud.cafe.domain.article.Article;
+import kr.codesqaud.cafe.domain.user.Member;
 import kr.codesqaud.cafe.repository.NamedJdbcTemplateArticleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import static kr.codesqaud.cafe.Constants.SESSIONED_USER;
+
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -16,37 +20,40 @@ import java.util.List;
 public class ArticleController {
 	private final NamedJdbcTemplateArticleRepository articleRepository;
 
-    private final Logger log = LoggerFactory.getLogger(ArticleController.class);
+	private final Logger log = LoggerFactory.getLogger(ArticleController.class);
 
-    @Autowired
-    public ArticleController(NamedJdbcTemplateArticleRepository articleRepository) {
-        this.articleRepository = articleRepository;
-    }
+	@Autowired
+	public ArticleController(NamedJdbcTemplateArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+	}
 
-    @GetMapping("/")
-    public String listArticles(Model model) throws SQLException {
-        List<Article> articles = articleRepository.showAllArticles();
-        model.addAttribute("articles", articles);
-        log.trace("게시글 갯수: {}", articles.size());
-        return "index";
-    }
+	@GetMapping("/")
+	public String listArticles(Model model) throws SQLException {
+		List<Article> articles = articleRepository.showAllArticles();
+		model.addAttribute("articles", articles);
+		log.trace("게시글 갯수: {}", articles.size());
+		return "index";
+	}
 
-    @GetMapping("/qna/questions")
-    public String writeForm() {
-        return "qna/form";
-    }
+	@GetMapping("/qna/questions")
+	public String writeForm(HttpSession session) {
+		if (session.getAttribute(SESSIONED_USER) == null) {
+			return "user/login";
+		}
+		return "qna/form";
+	}
 
-    @PostMapping("/qna/questions")
-    public String saveQuestion(@ModelAttribute("article") Article article) throws SQLException {
-        articleRepository.write(article);
-        return "redirect:/";
-    }
+	@PostMapping("/qna/questions")
+	public String saveQuestion(@ModelAttribute("article") Article article) throws SQLException {
+		articleRepository.write(article);
+		return "redirect:/";
+	}
 
-    @GetMapping("/articles/{articleSequence}")
-    public String articleShow(@PathVariable Long articleSequence, Model model) throws SQLException {
-        Article findArticle = articleRepository.findByArticleSequence(articleSequence);
-        model.addAttribute("article", findArticle);
-        log.trace("제목: {}, 글쓴이: {}, 내용: {}", findArticle.getTitle(), findArticle.getWriter(), findArticle.getContents());
-        return "qna/show";
-    }
+	@GetMapping("/articles/{articleSequence}")
+	public String articleShow(@PathVariable Long articleSequence, Model model) throws SQLException {
+		Article findArticle = articleRepository.findByArticleSequence(articleSequence);
+		model.addAttribute("article", findArticle);
+		log.trace("제목: {}, 글쓴이: {}, 내용: {}", findArticle.getTitle(), findArticle.getWriter(), findArticle.getContents());
+		return "qna/show";
+	}
 }
