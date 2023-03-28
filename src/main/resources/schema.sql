7da3eb8f454c83402c4f601ceb8fc030d021a5b5
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-28 23:56:27 +0900

    Refactor: schema.sql 코드 수정
    member 테이블의 member_id를 unique로 바꾸고 article 테이블에 foreign key로 member 테이블의 member_number를 가리키게 코드 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,24 @@
 drop table if exists article;
 drop table if exists member;
 
+
 create table member
 (
-    member_number   bigint      not null auto_increment,
-    member_id       varchar(64) not null,
+    member_number   bigint auto_increment primary key,
+    member_id       varchar(64) not null unique,
     member_password varchar(64) not null,
     member_name     varchar(64) not null,
-    member_email    varchar(64) not null,
-    primary key (member_id, member_number)
+    member_email    varchar(64) not null
 );
 
 create table article
 (
-    article_number      bigint        not null auto_increment,
-    article_writer      varchar(64)   not null,
+    article_number      bigint auto_increment primary key,
     article_title       varchar(200)  not null,
     article_contents    varchar(1000) not null,
     article_writtentime timestamp     not null default current_timestamp(),
-    primary key (article_number)
-
+    member_number       bigint,
+    foreign key (member_number) references member (member_number)
 );
+
+
