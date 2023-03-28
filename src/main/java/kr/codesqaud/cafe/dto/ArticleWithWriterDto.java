c6891386e46523c4e5f0d692e9d50b1147dbda17
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-29 00:05:08 +0900

    Feat: ArticleWithWriterDto.java 생성
    게시글의 작성자까지 필드로 담고 리스트에서 보일 때와 상세 열람 시에 정보를 넘겨주는 ArticleWithWriterDto.java 생성

--- src/main/java/kr/codesqaud/cafe/dto/ArticleWithWriterDto.java
+++ src/main/java/kr/codesqaud/cafe/dto/ArticleWithWriterDto.java
@@ -1,7 +1,60 @@
 package kr.codesqaud.cafe.dto;
 
+import java.time.LocalDateTime;
+
 public class ArticleWithWriterDto {
 
+	private Long articleSequence;
+	private String title;
+	private String contents;
+	private LocalDateTime writtenTime;
+	private Long userSequence;
 
+	public ArticleWithWriterDto(Long articleSequence, String title, String contents, LocalDateTime writtenTime, Long userSequence) {
+		this.articleSequence = articleSequence;
+		this.title = title;
+		this.contents = contents;
+		this.writtenTime = writtenTime;
+		this.userSequence = userSequence;
+	}
 
+	public Long getArticleSequence() {
+		return articleSequence;
+	}
+
+	public void setArticleSequence(Long articleSequence) {
+		this.articleSequence = articleSequence;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public LocalDateTime getWrittenTime() {
+		return writtenTime;
+	}
+
+	public void setWrittenTime(LocalDateTime writtenTime) {
+		this.writtenTime = writtenTime;
+	}
+
+	public Long getUserSequence() {
+		return userSequence;
+	}
+
+	public void setUserSequence(Long userSequence) {
+		this.userSequence = userSequence;
+	}
 }
