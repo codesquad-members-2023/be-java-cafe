6d02a53bb5b5b7713c53ce55703838b04d269750
saehim <saeilee.vxv@gmail.com>
2023-03-28 09:37:20 +0900

    Feat : `AuthorityInterceptor` 구현 `MvcConfig`에 등록

--- src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
+++ src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
@@ -1,12 +1,21 @@
 package kr.codesqaud.cafe.config;
 
+import kr.codesqaud.cafe.interceptor.AuthorityInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthorityInterceptor())
+                .addPathPatterns(AuthorityInterceptor.LOGIN_ESSENTIAL)
+                .excludePathPatterns(AuthorityInterceptor.LOGIN_INESSENTIAL);
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
