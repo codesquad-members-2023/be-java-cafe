a0c40923135e8e431c1292eb25ce12664972830d
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-28 13:59:04 +0900

    Feat: Constants.java 생성
    상수 처리한 변수들을 선언해놓은 Constants.java 생성

---
+++ src/main/java/kr/codesqaud/cafe/controller/Constants.java
@@ -0,0 +1,9 @@
+package kr.codesqaud.cafe.controller;
+
+public final class Constants {
+	private Constants() {
+		// restrict instantiation
+	}
+
+	public static final String SESSIONED_USER ="sessionedUser";
+}
