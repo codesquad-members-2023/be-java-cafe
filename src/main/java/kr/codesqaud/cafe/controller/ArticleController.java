23dd014531fcb026e741abbd9cdeec11b77dc501
Life is a ninanino <tvxqinak@naver.com>
2023-03-29 18:17:06 +0900

    refactor : 게시글에 댓글 추가
    게시글에 댓글 정보 추가

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -2,30 +2,31 @@ package kr.codesqaud.cafe.controller;
 
 import kr.codesqaud.cafe.domain.Article;
 import kr.codesqaud.cafe.domain.Member;
+import kr.codesqaud.cafe.domain.Reply;
 import kr.codesqaud.cafe.repository.article.JdbcArticleRepository;
+import kr.codesqaud.cafe.repository.reply.JdbcReplyRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+
 
 @Controller
 public class ArticleController {
 
     private JdbcArticleRepository jdbcArticleRepository;
+    private JdbcReplyRepository jdbcReplyRepository;
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
-    public ArticleController(JdbcArticleRepository jdbcArticleRepository) {
+    public ArticleController(JdbcArticleRepository jdbcArticleRepository, JdbcReplyRepository jdbcReplyRepository) {
         this.jdbcArticleRepository = jdbcArticleRepository;
+        this.jdbcReplyRepository = jdbcReplyRepository;
         logger.debug("articleController");
     }
 
@@ -46,19 +47,16 @@ public class ArticleController {
 
     @GetMapping("/articles/{id}")
     public String getContent(@PathVariable Long id, Model model) {
-        Optional<Article> article = jdbcArticleRepository.findById(id);
-        System.out.println(article.isPresent());
-        if (article.isPresent()) {
-            model.addAttribute("articles", article.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 게시글이 없습니다.")));
-            return "qna/show";
-        } else {
-            return "redirect:/";
-        }
+        Article article = jdbcArticleRepository.findById(id).orElseThrow();
+        List<Reply> replies = jdbcReplyRepository.findAll(article.getId());
+        model.addAttribute("replies",replies);
+        model.addAttribute("articles",article);
+        return "qna/show";
     }
+
 
     @GetMapping("/articles/{id}/form")
     public String updatePost(@PathVariable("id") Long id, Model model, HttpSession session) {
-        Object value = session.getAttribute("sessionedUser");
         Article writer = jdbcArticleRepository.findById(id).orElseThrow(null);
         vaildateAuthorization(session, writer);
         model.addAttribute("modified", writer);
@@ -80,7 +78,7 @@ public class ArticleController {
     }
 
     public void vaildateAuthorization(HttpSession session, Article article) {
-        Member value = (Member) session.getAttribute("sessionedUser");
+        Member value = (Member) session.getAttribute(MemberSessionUser.LOGIN_MEMBER);
         String loginId = value.getUserId();
         String writerId = article.getAuthorId();
         if (loginId != writerId) {
