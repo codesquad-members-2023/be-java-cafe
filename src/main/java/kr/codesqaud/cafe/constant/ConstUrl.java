eb0454347d66abaad9124ca62b0766747a154cab
saehim <saeilee.vxv@gmail.com>
2023-03-29 21:46:40 +0900

    Feat (answerController) : 댓글 생성/수정/삭제 컨트롤러 구현
    - 그러나 뷰가 없어서 수정은 실제로 동작해볼 수 없었다.

--- src/main/java/kr/codesqaud/cafe/constant/ConstUrl.java
+++ src/main/java/kr/codesqaud/cafe/constant/ConstUrl.java
@@ -2,4 +2,5 @@ package kr.codesqaud.cafe.constant;
 
 public abstract class ConstUrl {
     public static String REDIRECT_INDEX = "redirect:/";
+    public static String REDIRECT_ARTICLE = "redirect:/articles/{articleId}";
 }
