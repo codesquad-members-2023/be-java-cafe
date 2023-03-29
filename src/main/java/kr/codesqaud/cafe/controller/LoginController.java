290e07499679f772cc5b5700f5a3a0c3e5847215
Life is a ninanino <tvxqinak@naver.com>
2023-03-29 18:17:49 +0900

    Style : 로그아웃 클래스 변경
    멤버 컨트롤러 -> 로그인 컨트롤러로 위치 변경

--- src/main/java/kr/codesqaud/cafe/controller/LoginController.java
+++ src/main/java/kr/codesqaud/cafe/controller/LoginController.java
@@ -1,15 +1,17 @@
 package kr.codesqaud.cafe.controller;
 
 import kr.codesqaud.cafe.domain.Member;
-import kr.codesqaud.cafe.repository.Member.JdbcMemberRepository;
+import kr.codesqaud.cafe.repository.member.JdbcMemberRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -28,7 +30,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute("user") Member member, HttpSession session, Model model) {
+    public String login(@ModelAttribute("user") Member member, HttpServletRequest request,Model model) {
         logger.debug("login {} ", member);
         Member existUser = jdbcMemberRepository.findById(member.getUserId())
                 .filter(m -> m.getPassword().equals(member.getPassword()))
@@ -38,7 +40,18 @@ public class LoginController {
             model.addAttribute("notExist", "fail");
             return "user/login";
         }
-        session.setAttribute("sessionedUser", existUser);
+        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션을 반환
+        HttpSession session = request.getSession();
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(MemberSessionUser.LOGIN_MEMBER, existUser);
         return "redirect:/";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            session.invalidate();
+        }
+        return "user/login";
     }
 }
