8edc234d6ba38fbfb9e96c4acba2f1bfe8f2e18e
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-29 15:29:59 +0900

    articleId에 따라 댓글 가져오기

--- src/main/java/kr/codesqaud/cafe/repository/comment/DBCommentRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/comment/DBCommentRepository.java
@@ -1,13 +1,34 @@
 package kr.codesqaud.cafe.repository.comment;
 
 import kr.codesqaud.cafe.domain.Comment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.util.List;
 
+@Primary
+@Repository
 public class DBCommentRepository implements CommentRepository {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public DBCommentRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     @Override
     public List<Comment> findAllCommentsByArticleId(long articleId) {
-        return null;
+        String sql = "select writer, contents, createdAt from comment where articleId = ?";
+
+        log.info("댓글 목록 가져오기 실행");
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Comment.class), articleId);
     }
 }
