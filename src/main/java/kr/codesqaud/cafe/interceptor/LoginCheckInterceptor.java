87b6678c2e9f7daf7fc3f4e6d1c9f943abfe1710
Life is a ninanino <tvxqinak@naver.com>
2023-03-29 18:20:20 +0900

    Style : 변수 -> 상수로 변경
    sessionUser를 클래스로 분리해서 상수처럼 쓰일 수 있게 변경

--- src/main/java/kr/codesqaud/cafe/interceptor/LoginCheckInterceptor.java
+++ src/main/java/kr/codesqaud/cafe/interceptor/LoginCheckInterceptor.java
@@ -1,5 +1,6 @@
 package kr.codesqaud.cafe.interceptor;
 
+import kr.codesqaud.cafe.controller.MemberSessionUser;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +12,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
         HttpSession session = request.getSession(false);
-        if(session == null || session.getAttribute("sessionedUser") == null){
+        if(session == null || session.getAttribute(MemberSessionUser.LOGIN_MEMBER) == null){
             response.sendRedirect("/login"); // user/login이 아니여따..! url의 경로를 지정하는건데 html을 적어서 에러가 나고있었다
             return false;
         }
