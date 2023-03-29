1b944dd33ff5539809b8ae51924ec7a81470984c
saehim <saeilee.vxv@gmail.com>
2023-03-29 15:39:51 +0900

    Refactor : 시그니처 수정 / 사용하지 않는 라인 삭제

--- src/main/java/kr/codesqaud/cafe/exception/ExceptionStatus.java
+++ src/main/java/kr/codesqaud/cafe/exception/ExceptionStatus.java
@@ -2,7 +2,8 @@ package kr.codesqaud.cafe.exception;
 
 public enum ExceptionStatus {
     NO_SESSION_USER("로그인이 필요한 기능입니다."),
-    INVALID_MEMBER("로그인한 유저가 아닙니다."),
+    INVALID_MEMBER("로그인한 회원이 아닙니다."),
+    INVALID_WRITER("작성자만 수정/삭제 할 수 있습니다."),
 
     UPDATE_FAILED_WRONG_PASSWORD("비밀번호가 다릅니다."),
     MEMBER_NOT_FOUND("회원 정보를 다시 입력해주세요."),
