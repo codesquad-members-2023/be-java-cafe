037aaacee41a37280f51a36af1e1cc1334dc554f
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-29 18:49:18 +0900

    Refactor: NamedJdbcTemplateArticleRepository.java 코드 수정
    쓰이지 않는 import문 제거

--- src/main/java/kr/codesqaud/cafe/repository/NamedJdbcTemplateArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/NamedJdbcTemplateArticleRepository.java
@@ -9,8 +9,6 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository
