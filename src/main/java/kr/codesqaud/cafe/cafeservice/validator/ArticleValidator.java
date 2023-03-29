2ec520b5c9bd5253d009cf16dec08d6709a52d92
leegyeongwhan <leekhy02@naver.com>
2023-03-29 19:51:52 +0900

    feat: 멤머와 글이 null인지 검증하는 클래스 구현합니다.

--- src/main/java/kr/codesqaud/cafe/cafeservice/validator/ArticleValidator.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/validator/ArticleValidator.java
@@ -1,18 +1,31 @@
 package kr.codesqaud.cafe.cafeservice.validator;
 
+import kr.codesqaud.cafe.cafeservice.domain.Article;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 @Component
-public class ArticleValidator  implements Validator {
+public class ArticleValidator implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+        return Article.class.isAssignableFrom(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-
+        if (target == null) {
+            errors.rejectValue("article", "required.article");
+            return;
+        }
+        Article article = (Article) target;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "article", "required");
+        if (article.getTitle() == null) {
+            errors.rejectValue("title", "required.title");
+        }
+        if (article.getContent() == null) {
+            errors.rejectValue("content", "required.content");
+        }
     }
 }
--- src/main/java/kr/codesqaud/cafe/cafeservice/validator/MemberValidator.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/validator/MemberValidator.java
@@ -1,18 +1,34 @@
 package kr.codesqaud.cafe.cafeservice.validator;
 
+import kr.codesqaud.cafe.cafeservice.domain.Member;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 @Component
-public class MemberValidator  implements Validator {
+public class MemberValidator implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+        return Member.class.isAssignableFrom(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-
+        if (target == null) {
+            errors.rejectValue("member", "required.member");
+            return;
+        }
+        Member member = (Member) target;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "member", "required");
+        if (member.getUserName() == null) {
+            errors.rejectValue("userName", "required.title");
+        }
+        if (member.getEmail() == null) {
+            errors.rejectValue("email", "required.email");
+        }
+        if (member.getPassword() == null) {
+            errors.rejectValue("password", "required.password");
+        }
     }
 }
