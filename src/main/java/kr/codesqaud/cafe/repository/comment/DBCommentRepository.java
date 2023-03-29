f65fcb184fde928dfac329c33a9dc697e7dca5a2
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-29 15:49:41 +0900

    댓글 개수 가져오기 쿼리 생성

--- src/main/java/kr/codesqaud/cafe/repository/comment/DBCommentRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/comment/DBCommentRepository.java
@@ -26,9 +26,16 @@ public class DBCommentRepository implements CommentRepository {
 
     @Override
     public List<Comment> findAllCommentsByArticleId(long articleId) {
-        String sql = "select writer, contents, createdAt from comment where articleId = ?";
+        String sql = "select writer, contents, createdAt from comment where articleId = ? and deletedAt is null";
 
         log.info("댓글 목록 가져오기 실행");
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Comment.class), articleId);
+    }
+
+    @Override
+    public int findNumOfCommentsByArticleId(long articleId) {
+        String sql = "select count(0) as numOfComments from comment where articleId = ? and deletedAt is null";
+
+        return jdbcTemplate.queryForObject(sql, Integer.class, articleId);
     }
 }
