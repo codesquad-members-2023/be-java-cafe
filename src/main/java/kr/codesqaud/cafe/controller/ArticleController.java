316f47101564939bfc3021da9c10da7cfbd2011a
jaea-kim <jaeakim98@gmail.com>
2023-03-29 17:09:02 +0900

    url 주소 변경

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package kr.codesqaud.cafe.controller;
 
 import kr.codesqaud.cafe.domain.Article;
+import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.dto.ArticleForm;
 import kr.codesqaud.cafe.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ArticleController {
@@ -19,9 +22,10 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
-    @PostMapping("/questions")
-    public String post(ArticleForm form) {
-        Article article = new Article(form.getWriter(), form.getTitle(), form.getContent());
+    @PostMapping("/articles")
+    public String post(ArticleForm form, HttpSession session) {
+        User loginUser = (User) session.getAttribute(SessionConst.LOGIN_USER);
+        Article article = new Article(loginUser.getId(), form.getTitle(), form.getContent());
 
         articleRepository.save(article);
 
