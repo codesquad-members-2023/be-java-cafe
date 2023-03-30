54dd5a50c6bf5d9020df8b5cd8d705880716c473
leegyeongwhan <leekhy02@naver.com>
2023-03-30 12:23:52 +0900

    refactor: 피드백을 반영하여 인터셉터 수정
    이름, 로그, 수정합니다. 주석을 추가합니다.

--- src/main/java/kr/codesqaud/cafe/cafeservice/Interceptor/LoginInterceptor.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/Interceptor/LoginInterceptor.java
@@ -10,19 +10,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class CustomInterceptor implements HandlerInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
 
     private final Logger log = LoggerFactory.getLogger(MemberController.class);
 
+    //세션이 존재하지 않거나 세션에 저장된 로그인 정보가 없는 경우, 로그인이 되어 있지 않은 사용자로 판단하여 로그 출력 후, 로그인 페이지로 redirect 합니다.
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
 
-        log.info("login인터셉터{}", requestURI);
+        log.debug("login인터셉터{}", requestURI);
         HttpSession session = request.getSession(false);
 
         if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
-            log.info("미인증 사용자 요청");
+            log.debug("미인증 사용자 요청");
             response.sendRedirect("/login");
             return false;
         }
