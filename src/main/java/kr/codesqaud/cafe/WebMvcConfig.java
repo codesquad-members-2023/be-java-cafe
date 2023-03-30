d8d55a4a407c291cacc0969c93b4517d93250f71
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-30 15:55:15 +0900

    Feat: WebMvcConfig.java 생성
    새롭게 추가한 LoginInterceptor.java를 설정해주는 WebMvcConfig.java 생성

---
+++ src/main/java/kr/codesqaud/cafe/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package kr.codesqaud.cafe;
+
+import kr.codesqaud.cafe.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+		LoginInterceptor loginInterceptor = new LoginInterceptor();
+		registry.addInterceptor(loginInterceptor)
+			.addPathPatterns(loginInterceptor.loginNecessary)
+			.excludePathPatterns(loginInterceptor.loginUnnecessary);
+	}
+}
