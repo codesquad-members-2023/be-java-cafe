a850193f4b6eae027798e8eb86ee60ec3a70e377
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-30 17:20:57 +0900

    Feat: Reply.java 생성
    사용자의 답글인 Reply.java 생성

---
+++ src/main/java/kr/codesqaud/cafe/domain/reply/Reply.java
@@ -0,0 +1,39 @@
+package kr.codesqaud.cafe.domain.reply;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Reply {
+
+	private String contents;
+	private LocalDateTime writtenTime;
+	private Long userSequence;
+
+	public Reply() {
+
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getWrittenTime() {
+		return writtenTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+	}
+
+	public void setWrittenTime(LocalDateTime writtenTime) {
+		this.writtenTime = writtenTime;
+	}
+
+	public Long getUserSequence() {
+		return userSequence;
+	}
+
+	public void setUserSequence(Long userSequence) {
+		this.userSequence = userSequence;
+	}
+}
