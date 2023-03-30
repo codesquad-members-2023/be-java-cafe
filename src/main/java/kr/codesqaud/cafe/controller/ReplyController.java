8e4d1963eb05a283f7e629490eff53ba73166ef6
Hyun <ghkdgus29@naver.com>
2023-03-30 15:41:02 +0900

    feat: 댓글 삭제 싱글페이지 구현
    AJAX 통신을 통해 동적으로 댓글을 삭제하도록 구현

--- src/main/java/kr/codesqaud/cafe/controller/ReplyController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ReplyController.java
@@ -3,16 +3,17 @@ package kr.codesqaud.cafe.controller;
 import kr.codesqaud.cafe.SessionConstant;
 import kr.codesqaud.cafe.domain.Reply;
 import kr.codesqaud.cafe.domain.dto.ReplyWithUser;
+import kr.codesqaud.cafe.domain.dto.Result;
 import kr.codesqaud.cafe.repository.H2DBReplyRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
 public class ReplyController {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
@@ -25,7 +26,6 @@ public class ReplyController {
     }
 
     @PostMapping("/articles/{index}/replies")
-    @ResponseBody
     public ReplyWithUser reply(@PathVariable int index, @RequestParam String contents, HttpSession session) {
         Reply reply = new Reply(contents, (int) session.getAttribute(SessionConstant.LOGIN_USER_ID), index);
 
@@ -35,8 +35,21 @@ public class ReplyController {
                 contents, reply.getCreateDate());
     }
 
+//    @DeleteMapping("/articles/{articleId}/replies/{replyId}")
+//    public String delete(@PathVariable int articleId, @PathVariable int replyId, HttpSession session) {
+//        Reply reply = replyRepository.findById(replyId);
+//        int userId = (int) session.getAttribute(SessionConstant.LOGIN_USER_ID);
+//
+//        if (userId != reply.getUserId()) {
+//            throw new IllegalArgumentException("[ERROR] 자신이 작성하지 않은 댓글은 삭제할 수 없습니다.");
+//        }
+//
+//        replyRepository.delete(replyId);
+//        return "redirect:/articles/{articleId}";
+//    }
+
     @DeleteMapping("/articles/{articleId}/replies/{replyId}")
-    public String delete(@PathVariable int articleId, @PathVariable int replyId, HttpSession session) {
+    public Result delete(@PathVariable int articleId, @PathVariable int replyId, HttpSession session) {
         Reply reply = replyRepository.findById(replyId);
         int userId = (int) session.getAttribute(SessionConstant.LOGIN_USER_ID);
 
@@ -45,6 +58,13 @@ public class ReplyController {
         }
 
         replyRepository.delete(replyId);
-        return "redirect:/articles/{articleId}";
+        return new Result("성공");
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Result illegalApiExHandler(IllegalArgumentException e) {
+        return new Result("실패이유 : " + e.getMessage());
+    }
+
 }
