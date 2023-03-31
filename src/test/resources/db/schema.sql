6fa78ce92a12e356871c6b7194f1b7a5bf1ed809
saehim <saeilee.vxv@gmail.com>
2023-03-31 15:37:10 +0900

    Refactor : 시그니처 변경

--- src/test/resources/db/schema.sql
+++ src/test/resources/db/schema.sql
@@ -19,19 +19,20 @@ create table article (
                          user_id long not null,
                          created_at  timestamp default current_timestamp(),
                          updated_at  timestamp default current_timestamp(),
+                         is_deleted boolean default false,
                          constraint ARTICLE_MEMBER_ID_FK
                              foreign key (user_id) references member (id)
 );
 
 create table answer (
-                         id  long auto_increment primary key,
-                         contents    text not null,
-                         user_id long not null,
-                         article_id long not null,
-                         created_at  timestamp,
-                         updated_at  timestamp,
-                         constraint ANSWER_MEMBER_ID_FK
-                             foreign key (user_id) references member (id),
-                         constraint ANSWER_ARTICLE_ID_FK
+                        id  long auto_increment primary key,
+                        contents    text not null,
+                        user_id long not null,
+                        article_id long not null,
+                        created_at  timestamp,
+                        updated_at  timestamp,
+                        constraint ANSWER_MEMBER_ID_FK
+                            foreign key (user_id) references member (id),
+                        constraint ANSWER_ARTICLE_ID_FK
                             foreign key (article_id) references article (id)
 );
