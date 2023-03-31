5d6984eac2ff8c58a146ae15373e1e9ff3795309
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-31 16:04:34 +0900

    rebase하면서 발생한 중복된 코드 삭제

--- src/main/java/kr/codesqaud/cafe/util/ValidateConstant.java
+++ src/main/java/kr/codesqaud/cafe/util/ValidateConstant.java
@@ -3,13 +3,5 @@ package kr.codesqaud.cafe.util;
 public class ValidateConstant {
 
     public static final String UNKNOWN_USER = "권한이 없습니다. 로그인이 필요합니다.";
-<<<<<<< HEAD
-<<<<<<< HEAD
     public static final String NOT_YOURS = "권한이 없습니다. 본인의 글이 아닙니다.";
-=======
->>>>>>> 116e9e2 (로그인한 사용자 권한 부여)
-=======
-    public static final String NOT_YOURS = "권한이 없습니다. 본인의 글이 아닙니다.";
->>>>>>> b0c6365 (본인의 글만 삭제할 수 있도록 함)
-
 }
