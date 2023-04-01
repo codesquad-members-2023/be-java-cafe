a88fd6ae354f0da387e4e1f8404ce06dad24222d
leegyeongwhan <leekhy02@naver.com>
2023-04-01 23:44:36 +0900

    chore: member 닉네임 컬럼추가

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,23 +1,36 @@
+Drop table if exists reply;
 Drop table if exists article;
 Drop table if exists member;
 
 CREATE TABLE member
 (
     id           bigint AUTO_INCREMENT primary key,
-    user_name    varchar(255) not null,
-    password     varchar(255) not null,
-    email        varchar(255) not null,
-    created_date timestamp default now()
+    userName    varchar(64) not null unique ,
+    password     varchar(64) not null,
+    nickName varchar(64) not null,
+    email        varchar(64) not null
 );
 
 CREATE TABLE article
 (
     id           bigint AUTO_INCREMENT primary key,
     member_id    bigint,
-    writer       varchar(255),
+    writer       varchar(64),
     title        varchar(255),
     content      text,
     created_date timestamp default now(),
     reply_count  integer   default 0,
     FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE reply
+(
+    id   bigint AUTO_INCREMENT primary key,
+    member_id  INT NOT NULL,
+    article_id INT NOT NULL,
+    content TEXT,
+    reply_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted boolean not null,
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (article_id) REFERENCES article (id)
 );
