87120ea3a68ab64890cd167b83477ef6d7d476ce
leegyeongwhan <leekhy02@naver.com>
2023-04-01 18:40:21 +0900

    feat: 게시글 입력 수정 관련해 메서드 검증 코드 추가
    게시글 입력, 수정시 검증하는 코드들과 서비스 계층 추가로인해 의존성 부분을 수정합니다.

--- src/main/java/kr/codesqaud/cafe/cafeservice/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/controller/ArticleController.java
@@ -1,20 +1,26 @@
 package kr.codesqaud.cafe.cafeservice.controller;
 
 import kr.codesqaud.cafe.cafeservice.domain.Article;
-import kr.codesqaud.cafe.cafeservice.exhandler.exception.ArticleNotFoundException;
+import kr.codesqaud.cafe.cafeservice.domain.Reply;
+import kr.codesqaud.cafe.cafeservice.dto.ArticleDTO;
 import kr.codesqaud.cafe.cafeservice.repository.article.ArticleRepository;
+import kr.codesqaud.cafe.cafeservice.repository.reply.ReplyRepository;
+import kr.codesqaud.cafe.cafeservice.service.ArticleService;
+import kr.codesqaud.cafe.cafeservice.service.ReplyService;
 import kr.codesqaud.cafe.cafeservice.session.LoginSessionUtils;
 import kr.codesqaud.cafe.cafeservice.session.SessionConst;
+import kr.codesqaud.cafe.cafeservice.validator.ArticleValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 import static kr.codesqaud.cafe.cafeservice.session.LoginSessionUtils.*;
 
@@ -22,11 +28,22 @@ import static kr.codesqaud.cafe.cafeservice.session.LoginSessionUtils.*;
 public class ArticleController {
 
     private final ArticleRepository repository;
+    private final ArticleService service;
+    private final ReplyRepository replyRepository;
+    private final ReplyService replyService;
+    private final ArticleValidator articleValidator;
+
     private final Logger log = LoggerFactory.getLogger(ArticleController.class);
 
     @Autowired
-    public ArticleController(ArticleRepository repository) {
+    public ArticleController(ArticleRepository repository, ArticleService service,
+                             ReplyRepository replyRepository, ReplyService replyService,
+                             ArticleValidator articleValidator) {
         this.repository = repository;
+        this.service = service;
+        this.replyRepository = replyRepository;
+        this.replyService = replyService;
+        this.articleValidator = articleValidator;
     }
 
     @GetMapping("/")
@@ -37,7 +54,12 @@ public class ArticleController {
     }
 
     @PostMapping("/questions")
-    public String questions(@ModelAttribute Article article) {
+    public String questions(@ModelAttribute Article article, BindingResult bindingResult) {
+        articleValidator.validate(article, bindingResult);
+        if (bindingResult.hasErrors()) {
+            log.debug("errors = {}", bindingResult);
+            return "redirect:/";
+        }
         log.debug("article{}=", article);
         repository.save(article);
         return "redirect:/";
@@ -45,16 +67,25 @@ public class ArticleController {
 
     @GetMapping("/articles/{id}")
     public String showArticle(@PathVariable Long id, Model model) {
-        Article article = validFindById(id, "글을 찾을 수 없습니다.");
+        Article article = service.findById(id);
+        System.out.println("Article" + article);
+        List<Reply> reply = replyService.findReplyList(article.getId());
+        System.out.println(reply);
+
+        if (article == null) {
+            return "qna/show_filed";
+        }
+
         model.addAttribute("article", article);
+        model.addAttribute("reply", reply);
         return "qna/show";
     }
 
-    @DeleteMapping("/questions/{id}delete")
+    @DeleteMapping("/articles/{id}/delete")
     public String deleteArticle(@PathVariable Long id, HttpSession session) {
         //Todo 세션아이디(로그인아이디)  Article의 FK키 와 비교
+        Article article = service.findById(id);
         LoginSessionUtils sessionUtils = (LoginSessionUtils) session.getAttribute(SessionConst.LOGIN_MEMBER);
-        Article article = validFindById(id, "글을 찾을 수 없습니다.");
         sessionCheckId(article.getUserId(), sessionUtils);
         repository.delete(id);
         return "redirect:/";
@@ -64,22 +95,19 @@ public class ArticleController {
     public String updateArticleForm(@PathVariable Long id, Model model, HttpSession session) {
         LoginSessionUtils sessionUtils = (LoginSessionUtils) session.getAttribute(SessionConst.LOGIN_MEMBER);
         sessionCheckId(id, sessionUtils);
-        Article article = validFindById(id, "글을 찾을 수 없습니다.");
-        model.addAttribute("article", article);
+        Article findArticle = service.findById(id);
+        model.addAttribute("article", findArticle);
         return "qna/updateForm";
     }
 
     @PutMapping("/articles/{id}/update")
-    public String updateArticle(@RequestParam String title, @RequestParam String content,
+    public String updateArticle(@Valid @ModelAttribute("article") ArticleDTO articleDTO, BindingResult result,
                                 @PathVariable Long id) {
         //TODO 자기가 작성한 글이 아니면 수정 할 수 없다.
-        Article article = validFindById(id, "수정할 수 없습니다");
-        repository.update(article, title, content);
-        return "redirect:/";
-    }
+        articleValidator.validate(articleDTO, result);
 
-    private Article validFindById(Long id, String message) {
-        Optional<Article> findArticle = repository.findById(id);
-        return findArticle.orElseThrow(() -> new ArticleNotFoundException(message));
+        Article findArticle = service.findById(id);
+        repository.update(findArticle, articleDTO.getTitle(), articleDTO.getContent());
+        return "redirect:/";
     }
 }
