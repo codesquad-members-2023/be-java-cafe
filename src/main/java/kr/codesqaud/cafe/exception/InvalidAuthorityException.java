0fe88f4623f5e9128e57484369dedafc043da5cf
saehim <saeilee.vxv@gmail.com>
2023-04-02 18:06:19 +0900

    Refactor : 상수 문자열에 `final` 추가

--- src/main/java/kr/codesqaud/cafe/exception/InvalidAuthorityException.java
+++ src/main/java/kr/codesqaud/cafe/exception/InvalidAuthorityException.java
@@ -1,8 +1,8 @@
 package kr.codesqaud.cafe.exception;
 
 public class InvalidAuthorityException extends Exception {
-    public static String NO_SESSION_USER = "로그인이 필요한 기능입니다.";
-    public static String INVALID_MEMBER = "로그인한 유저가 아닙니다.";
+    public static final String NO_SESSION_USER = "로그인이 필요한 기능입니다.";
+    public static final String INVALID_MEMBER = "로그인한 유저가 아닙니다.";
 
     public InvalidAuthorityException(String status) {
         super(status);
