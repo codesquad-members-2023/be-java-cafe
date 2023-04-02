264d4d256aace551720825e16f882631dd4e4b41
leegyeongwhan <leekhy02@naver.com>
2023-04-02 18:32:00 +0900

    feat: 중복회원 검증로직 추가

--- src/main/java/kr/codesqaud/cafe/cafeservice/service/MemberService.java
+++ src/main/java/kr/codesqaud/cafe/cafeservice/service/MemberService.java
@@ -4,6 +4,7 @@ import kr.codesqaud.cafe.cafeservice.domain.Member;
 import kr.codesqaud.cafe.cafeservice.exhandler.exception.MemberNotFoundException;
 import kr.codesqaud.cafe.cafeservice.repository.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +19,14 @@ public class MemberService {
 
     public Member findById(Long id) {
         return memberRepository.findById(id).orElseThrow(() -> new MemberNotFoundException("member를 찾을수 없습니다"));
+    }
+
+    private boolean vaildMemberId(String userName) {
+        try {
+            memberRepository.findUserName(userName);
+        } catch (EmptyResultDataAccessException e) {
+            throw new MemberNotFoundException("중복 회원입니다.");
+        }
+        return true;
     }
 }
