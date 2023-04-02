30833fe51cf31bed945a672f9b7b7eabe9fa86a5
leegyeongwhan <leekhy02@naver.com>
2023-04-02 14:24:43 +0900

    feat: 로그인 dto 구현 아이디와 비밀번호만 구현.

---
+++ src/main/java/kr/codesqaud/cafe/cafeservice/dto/LoginDto.java
@@ -0,0 +1,32 @@
+package kr.codesqaud.cafe.cafeservice.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginDto {
+
+    @NotBlank
+    private String userId;
+    @NotBlank
+    private String password;
+
+    public LoginDto(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
