e37414ed1a5f82e4f6d66b8160ebdde88d56fd73
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-02 20:07:48 +0900

    Feat: ReplyService.java 댓글 검색, 수정, 삭제 기능 추가
    전반적인 댓글과 관련된 서비스를 제공하는 ReplyService.java에 특정 댓글을 조회하고 수정, 삭제하는 기능을 추가

--- src/main/java/kr/codesqaud/cafe/domain/reply/ReplyService.java
+++ src/main/java/kr/codesqaud/cafe/domain/reply/ReplyService.java
@@ -2,8 +2,11 @@ package kr.codesqaud.cafe.domain.reply;
 
 import kr.codesqaud.cafe.domain.user.Member;
 import kr.codesqaud.cafe.dto.ReplyDto;
+import kr.codesqaud.cafe.dto.ReplyEditDto;
 import kr.codesqaud.cafe.repository.NamedJdbcTemplateMemberRepository;
 import kr.codesqaud.cafe.repository.NamedJdbcTemplateReplyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,7 @@ public class ReplyService {
 
 	private final NamedJdbcTemplateReplyRepository replyRepository;
 	private final NamedJdbcTemplateMemberRepository memberRepository;
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	public ReplyService(NamedJdbcTemplateReplyRepository replyRepository, NamedJdbcTemplateMemberRepository memberRepository) {
@@ -32,9 +36,25 @@ public class ReplyService {
 		replyRepository.write(reply);
 	}
 
-	// 댓글 조회
+	// 해당 게시물의 모든 댓글 조회
 	public List<ReplyDto> showReplies(Long articleSequence) {
 		List<ReplyDto> allReplies = replyRepository.showAllReplies(articleSequence);
 		return allReplies;
+	}
+
+	// 특정 댓글 검색
+	public ReplyEditDto findReplyEditDto(Long replySequence) {
+		ReplyEditDto replyEditDto = replyRepository.findReplyEditDtoBySequence(replySequence);
+		return replyEditDto;
+	}
+
+	// 댓글 수정
+	public void editReply(ReplyEditDto replyEditDto) {
+		replyRepository.update(replyEditDto);
+	}
+
+	// 댓글 삭제
+	public void deleteReply(ReplyEditDto replyEditDto) {
+		replyRepository.delete(replyEditDto);
 	}
 }
