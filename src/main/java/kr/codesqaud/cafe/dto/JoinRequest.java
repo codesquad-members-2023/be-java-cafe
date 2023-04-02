634276d3f930236bd9b56e55186692558442b0dd
saehim <saeilee.vxv@gmail.com>
2023-04-03 00:10:37 +0900

    Fix (validation) : 예외메시지 수정

--- src/main/java/kr/codesqaud/cafe/dto/JoinRequest.java
+++ src/main/java/kr/codesqaud/cafe/dto/JoinRequest.java
@@ -7,21 +7,21 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class JoinRequest {
-    @NotNull(message = "아이디를 입력해주세요.")
-    @Size(min = 8, max = 80, message = "회원 아이디는 8~80자까지 가능합니다.")
+    @NotNull(message = "아이디를 입력해주세요. (8~80자)")
+    @Size(min = 8, max = 80)
     private String userId;
 
-    @NotNull(message = "비밀번호를 입력해주세요.")
-    @Size(min = 8, max = 200, message = "비밀번호는 8~200자까지 가능합니다.")
+    @NotNull(message = "비밀번호를 입력해주세요. (8~200자)")
+    @Size(min = 8, max = 200)
     private String password;
 
-    @NotNull(message = "닉네임을 입력해주세요.")
-    @Size(min = 2, max = 50, message = "닉네임은 2~50자까지 가능합니다.")
+    @NotNull(message = "닉네임을 입력해주세요. (2~50자)")
+    @Size(min = 2, max = 50)
     private String nickname;
 
-    @NotNull(message = "이메일을 입력해주세요.")
+    @NotNull(message = "이메일을 입력해주세요. (5~300자)")
     @Email(message = "이메일 형식에 맞게 해주세요.")
-    @Size(min = 5, max = 300, message = "이메일은 5~300자까지 가능합니다.")
+    @Size(min = 5, max = 300)
     private String email;
 
     public Member toEntity() {
