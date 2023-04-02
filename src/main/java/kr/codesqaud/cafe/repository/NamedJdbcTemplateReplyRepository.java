3cbd57c2a75dbcf0fca4c0c7c520e4fc4dbbf3c3
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-02 20:02:29 +0900

    Feat: NamedJdbcTemplateReplyRepository.java 댓글 수정 및 삭제 기능을 위한 SQL문 추가 작성
    댓글 수정 및 삭제 기능을 위한 SQL문 추가 작성

--- src/main/java/kr/codesqaud/cafe/repository/NamedJdbcTemplateReplyRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/NamedJdbcTemplateReplyRepository.java
@@ -3,6 +3,7 @@ package kr.codesqaud.cafe.repository;
 
 import kr.codesqaud.cafe.domain.reply.Reply;
 import kr.codesqaud.cafe.dto.ReplyDto;
+import kr.codesqaud.cafe.dto.ReplyEditDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -26,24 +27,53 @@ public class NamedJdbcTemplateReplyRepository {
 	public void write(Reply reply) {
 		SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(reply);
 		template.update("insert into reply(reply_contents, reply_writer, reply_member_number, reply_article_number) values (:contents, :writer, :userSequence, :articleSequence)", sqlParameterSource);
-		System.out.println("--> 댓글 작성자 번호: "+reply.getUserSequence());
 	}
 
 	public List<ReplyDto> showAllReplies(Long articleSequence) {
-		String sql = "select r.reply_article_number, r.reply_writer, r.reply_member_number, a.member_number, r.reply_contents, r.reply_writtentime from reply r join article a on r.reply_article_number=a.article_number where r.reply_article_number=:articleSequence";
+		String sql = "select r.reply_sequence, r.reply_article_number, r.reply_writer, r.reply_member_number, a.member_number, r.reply_contents, r.reply_writtentime from reply r join article a on r.reply_article_number=a.article_number where r.reply_article_number=:articleSequence";
 		SqlParameterSource sqlParameterSource = new MapSqlParameterSource("articleSequence", articleSequence);
 		return template.query(sql, sqlParameterSource, replyDtoRowMapper());
 	}
 
+	public ReplyEditDto findReplyEditDtoBySequence(Long replySequence) {
+		String sql = "select reply_contents, reply_member_number, reply_sequence from reply where reply_sequence=:replySequence";
+		SqlParameterSource sqlParameterSource = new MapSqlParameterSource("replySequence", replySequence);
+		return template.queryForObject(sql, sqlParameterSource, replyOnlyWithContentsDtoRowMapper());
+	}
+
+	public void update(ReplyEditDto replyEditDto) {
+		String sql = "update reply set reply_contents=:contents where reply_sequence=:replySequence";
+		SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(replyEditDto);
+		template.update(sql, sqlParameterSource);
+	}
+
+	public void delete(ReplyEditDto replyEditDto) {
+		String sql = "delete from reply where reply_sequence=:replySequence";
+		SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(replyEditDto);
+		template.update(sql, sqlParameterSource);
+	}
+
+
 	private RowMapper<ReplyDto> replyDtoRowMapper() {
 		return (rs, rowNum) -> {
 			ReplyDto replyDto = new ReplyDto();
+			replyDto.setReplySequence(rs.getLong("reply_sequence"));
 			replyDto.setWriter(rs.getString("reply_writer"));
 			replyDto.setUserSequence(rs.getLong("reply_member_number"));
 			replyDto.setContents(rs.getString("reply_contents"));
 			replyDto.setWrittenTime(rs.getTimestamp("reply_writtentime").toLocalDateTime());
 			replyDto.setArticleSequence(rs.getLong("reply_article_number"));
 			return replyDto;
+		};
+	}
+
+	private RowMapper<ReplyEditDto> replyOnlyWithContentsDtoRowMapper() {
+		return (rs, rowNum) -> {
+			ReplyEditDto replyEditDto = new ReplyEditDto();
+			replyEditDto.setContents(rs.getString("reply_contents"));
+			replyEditDto.setReplySequence(rs.getLong("reply_sequence"));
+			replyEditDto.setUserSequence(rs.getLong("reply_member_number"));
+			return replyEditDto;
 		};
 	}
 }
